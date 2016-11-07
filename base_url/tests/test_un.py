466a642507047440373523c3f12d4c1447c26716
MonsieurB <contact@ebii.fr>
2016-11-07 14:25:00 +0100

    header

--- base_url/tests/test_un.py
+++ base_url/tests/test_un.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+#    Copyright (C) 2013 Akretion (http://www.akretion.com)
+#    @author EBII MonsieurB <monsieurb@saaslys.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from openerp.tests.common import SingleTransactionCase
 import logging
 _logger = logging.getLogger(__name__)
