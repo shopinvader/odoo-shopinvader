86058edfc8f6234526e7237e8735b6f5a9cebb37
MonsieurB <contact@ebii.fr>
2016-11-08 18:01:28 +0100

    test onchange_catagory_url_key

--- product_url/tests/test_category.py
+++ product_url/tests/test_category.py
@@ -43,6 +43,17 @@ class Testbaseurlcategory(SingleTransactionCase):
         _logger.info(u"url_key : %s ", url_key)
 
         self.assertEqual('un-joli-epervier', url_key)
+    def test_onchange_category_url_key(self):
+        category = self.env['product.category'].browse(2)
+
+        #import pdb; pdb.set_trace()
+        category.url_key = u"Un Joli épervier"
+        res = category.on_url_key_change()
+
+        _logger.debug(res['warning'],)
+
+        self.assertEqual('it will will be adapted to un-joli-epervier',
+                         res['warning']['message'])
 
     def test_get_object(self):
         category = self.env['product.category'].browse(2)
