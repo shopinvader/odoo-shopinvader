2f3d1b7e5c8d0354ac120c05e6c84449e89f3c0e
MonsieurB <contact@ebii.fr>
2016-11-09 20:07:40 +0100

    AH PAS VU

--- base_url/__openerp__.py
+++ base_url/__openerp__.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
 #    Rewrite url Base module for OpenERP
 #    Copyright (C) 2013 Akretion (http://www.akretion.com)
 #    @author EBII MonsieurB <monsieurb@saaslys.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-#
-##############################################################################
 
 {
     'name': 'Base Url',
