94f986e4d9e12aa605e79a67ef72b15dbfdc1e10
MonsieurB <eric@ebii.fr>
2016-11-25 13:01:24 +0100

    one line code maniac change & test

--- product_url/tests/test_category.py
+++ product_url/tests/test_category.py
@@ -4,83 +4,73 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class Testbaseurlcategory(SingleTransactionCase):
+class TestBaseUrlCategory(SingleTransactionCase):
 
-    def setup(self):
-        super(Testbaseurlcategory, self).setup()
+    def setUp(self):
+        super(TestBaseUrlCategory, self).setUp()
+        self.category = self.env.ref('product.product_category_2')
 
     def test__get_reference(self):
-        category = self.env['product.category'].browse(2)
+        category = self.category
         model_ref = category._get_model_id_reference()
         model = "%s,%s" % (category._name, category.id)
 
         self.assertEqual(model, model_ref)
-        self.assertEqual("product.category,2", model_ref)
+        self.assertEqual("product.category,3", model_ref)
 
     def test_change_name(self):
-        category = self.env['product.category'].browse(2)
+        category = self.category
 
         category.name = u"Un Joli épervier"
 
         category.on_name_change()
 
-        # product._inverse_set_url()
-
         np = self.env['url.url'].search(
-            [('model_id', '=', "product.category,2"),
+            [('model_id', '=', "product.category,3"),
              ('redirect', '=', False)])
         _logger.info(u"url_key len  : %s ", len(np))
 
         url_key = np.url_key
         _logger.info(u"url_key : %s ", url_key)
-        self.assertTrue(len(np) == 1)
+        self.assertEqual(len(np), 1)
         self.assertEqual('un-joli-epervier', url_key)
 
     def test_change_url_key(self):
-        category = self.env['product.category'].browse(2)
+        category = self.category
 
         category.url_key = u"Un Joli épervier"
         category.on_url_key_change()
-        category._inverse_set_url()
 
-        url_key = self.env['url.url'].search([
-            ('model_id', '=', "product.category,2"),
-            ('redirect', '=', False)]).url_key
+        _logger.info(u"url_key : %s ", category.url_key)
 
-        _logger.info(u"url_key : %s ", url_key)
-
-        self.assertEqual('un-joli-epervier', url_key)
+        self.assertEqual('un-joli-epervier', category.url_key)
 
     def test_onchange_category_url_key(self):
-        category = self.env['product.category'].browse(2)
+        category = self.category
 
         category.url_key = u"Un Joli épervier"
         res = category.on_url_key_change()
 
         _logger.debug(res['warning'],)
 
-        self.assertEqual('it will will be adapted to un-joli-epervier',
+        self.assertEqual('it will be adapted to un-joli-epervier',
                          res['warning']['message'])
 
     def test_get_object(self):
-        category = self.env['product.category'].browse(2)
+        category = self.category
         category.name = u"Un Joli épervier"
 
         category.on_name_change()
-        category._inverse_set_url()
-        url1 = self.env['url.url'].search([
-            ('model_id', '=', "product.category,2"),
-            ('redirect', '=', False)]).url_key
+        url1 = category.url_key
+        _logger.info("URL1: %s " % (url1))
 
         category.url_key = u"De Jolie éperviere"  # (de-jolie-eperviere)
-        category._inverse_set_url()
-        url2 = self.env['url.url'].search([
-            ('model_id', '=', "product.category,2"),
-            ('redirect', '=', False)]).url_key
+        category.on_url_key_change()
+        url2 = category.url_key
+        _logger.info("URL2: %s " % (url2))
 
         _logger.info("les url: %s %s " % (url1, url2))
         urlurl1 = self.env['url.url']._get_object(url1)
-
         urlurl2 = self.env['url.url']._get_object(url2)
 
         self.assertEqual(urlurl1, urlurl2)
--- product_url/tests/test_product.py
+++ product_url/tests/test_product.py
@@ -5,13 +5,15 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class Testbaseurl(SingleTransactionCase):
+class TestBaseUrl(SingleTransactionCase):
 
-    def setup(self):
-        super(Testbaseurl, self).setup()
+    def setUp(self):
+        super(TestBaseUrl, self).setUp()
+        self.prod2 = self.env.ref('product.product_product_1_product_template')
+        self.prod9 = self.env.ref('product.product_product_7_product_template')
 
     def test__get_reference(self):
-        product = self.env['product.template'].browse(9)
+        product = self.prod9
         model_ref = product._get_model_id_reference()
         model = "%s,%s" % (product._name, product.id)
         self.assertEqual(model, model_ref)
@@ -23,21 +25,18 @@ class Testbaseurl(SingleTransactionCase):
         self.assertEqual(model_ref, "product.template,2")
 
     def test_get_url(self):
-        product = self.env['product.template'].browse(2)
+        product = self.prod2
         self.assertEqual("good-monitoring", product.url_key)
 
     def test_get_redirect_url(self):
-        product = self.env['product.template'].browse(2)
+        product = self.prod2
         self.assertEqual(4, len(product.redirect_url_key_ids))
 
     def test_change_name(self):
-        product = self.env['product.template'].browse(9)
+        product = self.prod9
 
         product.name = u"Un Joli épervier"
-
         product.on_name_change()
-
-        # product._inverse_set_url()
 
         np = self.env['url.url'].search(
             [('model_id', '=', "product.template,9"),
@@ -46,55 +45,45 @@ class Testbaseurl(SingleTransactionCase):
 
         url_key = np.url_key
         _logger.info(u"url_key : %s ", url_key)
-        self.assertTrue(len(np) == 1)
+        self.assertEqual(len(np), 1)
         self.assertEqual('un-joli-epervier', url_key)
 
     def test_change_url_key(self):
-        product = self.env['product.template'].browse(9)
+        product = self.prod9
 
         product.url_key = u"Un Joli épervier"
         product.on_url_key_change()
 
-        product._inverse_set_url()
+        _logger.info(u"url_key : %s ", product.url_key)
 
-        url_key = self.env['url.url'].search([
-            ('model_id', '=', "product.template,9"),
-            ('redirect', '=', False)]).url_key
-
-        _logger.info(u"url_key : %s ", url_key)
-
-        self.assertEqual('un-joli-epervier', url_key)
+        self.assertEqual('un-joli-epervier', product.url_key)
 
     def test_onchange_url_key(self):
-        product = self.env['product.template'].browse(9)
+        product = self.prod9
 
         product.url_key = u"Un Joli épervier"
         res = product.on_url_key_change()
 
         _logger.debug(res['warning'],)
 
-        self.assertEqual('it will will be adapted to un-joli-epervier',
+        self.assertEqual('it will be adapted to un-joli-epervier',
                          res['warning']['message'])
 
     def test_get_object(self):
-        product = self.env['product.template'].browse(9)
+        product = self.prod9
         product.name = u"Un Joli épervier"
 
         product.on_name_change()
-        product._inverse_set_url()
-        url1 = self.env['url.url'].search([
-            ('model_id', '=', "product.template,9"),
-            ('redirect', '=', False)]).url_key
+        url1 = product.url_key
+        _logger.info("URL1: %s " % (url1))
 
         product.url_key = u"De Jolie éperviere"  # (de-jolie-eperviere)
-        product._inverse_set_url()
-        url2 = self.env['url.url'].search([
-            ('model_id', '=', "product.template,9"),
-            ('redirect', '=', False)]).url_key
-
+        product.on_url_key_change()
+        url2 = product.url_key
+        _logger.info("URL2: %s " % (url2))
         _logger.info("les url: %s %s " % (url1, url2))
-        urlurl1 = self.env['url.url']._get_object(url1)
 
+        urlurl1 = self.env['url.url']._get_object(url1)
         urlurl2 = self.env['url.url']._get_object(url2)
 
         self.assertEqual(urlurl1, urlurl2)
