042d8f7da9b8d13c7f803aa67452314b106b685e
MonsieurB <contact@ebii.fr>
2016-12-01 16:48:34 +0100

    dernier contrôle avant

--- delivery_data/__openerp__.py
+++ delivery_data/__openerp__.py
@@ -16,7 +16,10 @@
     # any module necessary for this one to work correctly
     'depends': ['delivery'],
 
-    'data': ['delivery_la_poste.xml',
+    'data': [
+             ],
+
+    'demo': ['delivery_la_poste.xml',
              'delivery_om1.xml',
              'delivery_om2.xml',
              'delivery_inter_z1.xml',
@@ -29,10 +32,7 @@
              'delivery_inter_z8.xml',
              'delivery_from_factory.xml',
              'delivery_tnt_24h.xml',
-             'delivery.grid.csv',
-             ],
-
-    'demo': [],
+             'delivery.grid.csv',],
 
     'url': '',
     'installable': True,
