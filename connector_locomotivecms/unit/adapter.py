0023a73d3fdddcb7033987add00cd5e451887471
Sébastien BEAU <sebastien.beau@akretion.com>
2017-01-05 14:11:05 +0100

    [IMP] add asset export support, not yet finish

--- connector_locomotivecms/unit/adapter.py
+++ connector_locomotivecms/unit/adapter.py
@@ -13,6 +13,8 @@ _logger = logging.getLogger(__name__)
 #### TODO create a python lib for that code
 import requests
 import json
+import StringIO
+import base64
 
 
 class LocomotiveApiError(Exception):
@@ -24,13 +26,12 @@ class LocomotiveApiError(Exception):
         self.extra = response
 
 
-class LocomotiveContent(object):
+class LocomotiveResource(object):
 
-    def __init__(self, client, content_type):
+    def __init__(self, client):
         self.client = client
-        self.content_type = content_type
-        self.path = '/content_types/%s/entries' % content_type
         self.call = self.client.call
+        self.path = None
 
     def _path_with_slug(self, id_or_slug):
         return self.path + '/' + id_or_slug
@@ -51,9 +52,35 @@ class LocomotiveContent(object):
     def create(self, data):
         return self.call('post', self.path, data={'content_entry': data})
 
-    def add(self, data):
-        print data
-        return self.update(data['odoo_id'], data=data)
+
+class LocomotiveContent(LocomotiveResource):
+
+    def __init__(self, client, content_type):
+        super(LocomotiveContent, self).__init__(client)
+        self.path = '/content_types/%s/entries' % content_type
+
+
+class LocomotiveAsset(LocomotiveResource):
+
+    def __init__(self, client):
+        super(LocomotiveAsset, self).__init__(client)
+        self.path = '/content_assets'
+
+    # TODO FIXME miss the key 'content_asset'
+    def write(self, id_or_slug, data):
+        f = StringIO.StringIO()
+        f.write(base64.b64decode(data['base64']))
+        f.seek(0)
+        return self.call('put', self._path_with_slug(id_or_slug),
+            files={'source': (data['filename'], f)})
+
+    # TODO FIXME miss the key 'content_asset'
+    def create(self, data):
+        f = StringIO.StringIO()
+        f.write(base64.b64decode(data['base64']))
+        f.seek(0)
+        return self.call('post', self.path,
+            files={'source': (data['filename'], f)})
 
 
 class LocomotiveClient(object):
@@ -78,15 +105,18 @@ class LocomotiveClient(object):
             'X-Locomotive-Account-Email': self.email,
             'X-Locomotive-Account-Token': self.token,
             'X-Locomotive-Site-Handle': self.handle,
+            #'Content-Disposition': 'form-data',
+            #'name': "content_asset[source]",
             }
 
-    def call(self, method, url, data=None):
+    def call(self, method, url, data=None, files=None):
         kwargs = {'headers': self.header()}
         if method == 'get':
             kwargs['params'] = data
         else:
             kwargs['json'] = data
-        res = getattr(requests, method)(self.url + url, json=data, headers=self.header())
+            kwargs['files'] = files
+        res = getattr(requests, method)(self.url + url, **kwargs)
         if res.status_code/100 != 2:
             raise LocomotiveApiError(res.json(), res.status_code)
         return res.json()
@@ -94,14 +124,14 @@ class LocomotiveClient(object):
     def content(self, content_type):
         return LocomotiveContent(self, content_type)
 
+    def asset(self):
+        return LocomotiveAsset(self)
+
+
 #####
 
 
-
-class LocomotivecmsAdapter(CRUDAdapter):
-    _model_name = None
-
-    __pool = {}  # pool of connection
+class LocomotiveAdapter(CRUDAdapter):
 
     def __init__(self, connector_env):
         backend = connector_env.backend_record
@@ -115,12 +145,25 @@ class LocomotivecmsAdapter(CRUDAdapter):
         self.client = client
 
     def create(self, vals):
-        res = self.client.content('products').create(vals)
-        return res['_id']
+        return self.resource.create(vals)
 
     def write(self, external_id, vals):
-        res = self.client.content('products').write(external_id, vals)
-        return res['_id']
+        return self.resource.write(external_id, vals)
 
     def delete(self, binding_id):
-        self.client.content('products').delete(binding_id)
+        self.resource.delete(binding_id)
+
+
+class LocomotiveContentAdapter(LocomotiveAdapter):
+    _content_type = None
+
+    def __init__(self, connector_env):
+        super(LocomotiveContentAdapter, self).__init__(connector_env)
+        self.resource = self.client.content(self._content_type)
+
+
+class LocomotiveAssetAdapter(LocomotiveAdapter):
+
+    def __init__(self, connector_env):
+        super(LocomotiveAssetAdapter, self).__init__(connector_env)
+        self.resource = self.client.asset()
