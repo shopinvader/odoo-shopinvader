7fb82d213172777c6fa81b6618d98de36a9ed339
Sébastien BEAU <sebastien.beau@akretion.com>
2017-01-05 14:01:29 +0100

    [IMP] customise the locomotive exporter in order to be able to process the information return by the api. Indeed when you create an image you may need to retrieve the url of the image pushed

--- connector_locomotivecms/unit/exporter.py
+++ connector_locomotivecms/unit/exporter.py
@@ -8,6 +8,7 @@ from openerp.addons.connector_generic.unit.exporter import GenericExporter
 from ..related_action import unwrap_binding
 from openerp.addons.connector.queue.job import job, related_action
 from ..connector import get_environment
+from openerp.tools.translate import _
 
 
 class LocomotivecmsExporter(GenericExporter):
@@ -17,6 +18,40 @@ class LocomotivecmsExporter(GenericExporter):
         """ Get the data to pass to :py:meth:`_update` """
         # As we push a json we always need to push all info
         return self._create_data(map_record, fields=None, **kwargs)
+
+    def _run(self, fields=None):
+        """ Flow of the synchronization, implemented in inherited classes"""
+        assert self.binding_id
+        assert self.binding_record
+
+        if not self.external_id:
+            fields = None  # should be created with all the fields
+
+        if self._has_to_skip():
+            return
+
+        # export the missing linked resources
+        self._export_dependencies()
+
+        # prevent other jobs to export the same record
+        # will be released on commit (or rollback)
+        self._lock()
+
+        map_record = self._map_data()
+
+        if self.external_id:
+            record = self._update_data(map_record, fields=fields)
+            if not record:
+                return _('Nothing to export.')
+            self.result = self._update(record)
+        else:
+            record = self._create_data(map_record, fields=fields)
+            if not record:
+                return _('Nothing to export.')
+            self.result = self._create(record)
+            self.external_id = self.result['_id']
+        return _('Record exported with ID %s on %s.') % (
+	    self.external_id, self.backend_record.name)
 
 
 @job(default_channel='root.locomotive')
