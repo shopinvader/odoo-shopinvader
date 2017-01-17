9278930675d7ad9f8f4b7d85cfc3d3d1d0c34bd2
Sébastien BEAU <sebastien.beau@akretion.com>
2017-01-17 09:00:23 +0100

    [FIX] fix previous commit add missing file

---
+++ shoptor/unit/deleter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp.addons.connector_locomotivecms.backend import locomotivecms
+from openerp.addons.connector_locomotivecms.unit.deleter import (
+    LocomotivecmsDeleter)
+
+
+@locomotivecms
+class ProductDeleter(LocomotivecmsDeleter):
+    _model_name = 'locomotivecms.product'
