d15564dbb5da98d5f2c033411cbc0f84a7b75277
Sébastien BEAU <sebastien.beau@akretion.com>
2017-06-01 02:24:08 +0200

    [FIX] fix test

--- shopinvader_m2mcategories/tests/test_product.py
+++ shopinvader_m2mcategories/tests/test_product.py
@@ -12,8 +12,8 @@ class ProductCase(ProductCommonCase):
     def test_one_categories(self):
         self.backend.bind_all_category()
         self.assertEqual(
-            self.template.categ_id,
-            self.shopinvader_variant.categories.record_id)
+            self.template.categ_id + self.template.categ_id.parent_id,
+            self.shopinvader_variant.categories.mapped('record_id'))
 
     def test_multi_categories(self):
         self.backend.bind_all_category()
