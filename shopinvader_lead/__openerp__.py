d73b7f6dd047de65d65942e818a14b5e9b1863f4
Sébastien BEAU <sebastien.beau@akretion.com>
2017-06-30 12:51:25 +0200

    [ADD] add shopinvader_lead

---
+++ shopinvader_lead/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Shopinvader Lead",
+    "summary": "Shopinvader Lead Management",
+    "version": "8.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://akretion.com",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "shopinvader",
+        "crm",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
