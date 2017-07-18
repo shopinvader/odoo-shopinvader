2b460e46af07083d7fa3817a0746d043b1dc5fe6
Sébastien BEAU <sebastien.beau@akretion.com>
2017-07-18 18:33:56 +0200

    [FIX] fix expoporting link between product

--- shopinvader_product_link/models/product.py
+++ shopinvader_product_link/models/product.py
@@ -9,14 +9,14 @@ from openerp import fields, models
 class ShopinvaderVariant(models.Model):
     _inherit = 'shopinvader.variant'
 
-    cross_sellings = fields.Many2many(
-        comodel_name='product.template',
+    cross_selling_ids = fields.Many2many(
+        comodel_name='shopinvader.variant',
         compute='_compute_link')
-    relateds = fields.Many2many(
-        comodel_name='product.template',
+    related_ids = fields.Many2many(
+        comodel_name='shopinvader.variant',
         compute='_compute_link')
-    up_sellings = fields.Many2many(
-        comodel_name='product.template',
+    up_selling_ids = fields.Many2many(
+        comodel_name='shopinvader.variant',
         compute='_compute_link')
 
     def _get_related_product(self, link_type):
@@ -27,11 +27,14 @@ class ShopinvaderVariant(models.Model):
                 for binding in\
                         link.linked_product_tmpl_id.shopinvader_bind_ids:
                     if binding.backend_id == self.backend_id:
-                        res.append(link.linked_product_tmpl_id.id)
+                        variants = [(v.record_id.sequence, v)
+                                    for v in binding.shopinvader_variant_ids]
+                        variants.sort()
+                        res.append(variants[0][1].id)
         return res
 
     def _compute_link(self):
         for record in self:
-            record.cross_sellings = record._get_related_product('cross_sell')
-            record.relateds = record._get_related_product('related')
-            record.up_sellings = record._get_related_product('up_sell')
+            record.cross_selling_ids = record._get_related_product('cross_sell')
+            record.related_ids = record._get_related_product('related')
+            record.up_selling_ids = record._get_related_product('up_sell')
