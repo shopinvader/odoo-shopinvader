af28df70366f11ef7c455b08d9d0de3642c2da93
Sébastien BEAU <sebastien.beau@akretion.com>
2017-07-24 15:50:49 +0200

    [IMP] add module partner_contact_company to simplify the management of address, make easier to delivery to a different company, or to our office

---
+++ partner_contact_company/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Company",
+    "summary": "Partner Company",
+    "version": "8.1.0.0",
+    "category": "Partner",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/partner_view.xml",
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
