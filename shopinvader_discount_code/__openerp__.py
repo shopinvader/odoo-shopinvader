6ae9999f123b34652e8c10fc3a2753818e8ffe47
Benoit <benoit.guillot@akretion.com.br>
2017-09-25 11:32:15 +0200

    [IMP] add module to manage discount code

---
+++ shopinvader_discount_code/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com)
+# Benoît GUILLOT <benoit.guillot@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "shopvinvader_discount_code",
+    "summary": "Module to manage discount code with shopinvader",
+    "version": "8.0.1.0.0",
+    "category": "Sale",
+    "website": "https://akretion.com",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
---
+++ shopinvader_discount_code/service/cart.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com)
+# @author Benoît GUILLOT <benoit.guillot@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.shopinvader.services.helper import to_int, secure_params
+from openerp.shopinvader.services.cart import CartService
+from openerp.addons.shopinvader.backend import shopinvader
+
+
+@shopinvader
+class DiscountService(CartService):
+    _model_name = 'sale.order'
+
+    # The following method are 'public' and can be called from the controller.
+    # All params are untrusted so please check it !
+
+    @secure_params
+    def update(self, params):
+        res = super(DiscountService, self).update(params)
+        if params.get('discount_code'):
+            cart = self._get()
+            cart.apply_discount()
+        return res
+
+    # Validator
+    def _validator_update(self):
+        res = super(DiscountService, self)._validator_update()
+        res['discount_code']: {'type': 'string'}
+        return res
+
+    def _parser(self):
+        res = super(DiscountService, self)._parser()
+        res.append('discount_code')
+        return res
