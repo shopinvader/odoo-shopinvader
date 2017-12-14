945c7d2d3956670e9693472e4ac8bc256f2cc4a8
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-12-14 11:44:56 +0100

    [ADD] setup.py

--- shopinvader/__manifest__.py
+++ shopinvader/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader",
     "summary": "Shopinvader",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "e-commerce",
     "website": "https://akretion.com",
     "author": "Akretion",
