3b67027430e44c912f02fda188fe8e585fadccde
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-01-09 10:53:47 +0100

    [MIG] shopinvader_search_engine to 10.0

--- shopinvader_search_engine/models/product.py
+++ shopinvader_search_engine/models/product.py
@@ -28,7 +28,7 @@ class ShopinvaderVariant(models.Model):
                 if len(fposition) > 0:
                     fposition = fposition[0]
                 record.price[role.code] = record._get_price(
-                    role.pricelist_id, fposition)
+                    role.pricelist_id, fposition, record.backend_id.company_id)
 
     @api.depends('lang_id', 'backend_id.se_backend_id')
     def _compute_index(self):
