fab8f5a8dccfc202cbc8fbe6348a3cf893b26463
Sébastien BEAU <sebastien.beau@akretion.com>
2018-03-06 16:33:50 +0100

    [REF] rename module shopinvader_profile to shopinvader_demo_app

--- shopinvader_demo_app/__manifest__.py
+++ shopinvader_demo_app/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-{'name': 'Shopinvader Profile',
+{'name': 'Shopinvader Demo App',
  'version': '10.0.1.0.0',
  'author': 'Akretion',
  'website': 'www.akretion.com',
@@ -23,5 +23,5 @@
  'demo': [
  ],
  'installable': True,
- 'application': False,
+ 'application': True,
  }
