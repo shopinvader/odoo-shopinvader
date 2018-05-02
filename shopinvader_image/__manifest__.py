aca969db359eaf6008e61a41b6b6e2b523635375
François Honoré <francois.honore@acsone.eu>
2018-05-02 16:55:47 +0200

    [ADD] First draft to have image into dict/json exported

---
+++ shopinvader_image/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion (http://www.akretion.com)
+# Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Shopinvader image",
+    "summary": "Shopinvader image (todo)",
+    "version": "10.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://akretion.com",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "depends": [
+        'shopinvader',
+        'shopinvader_algolia',
+        'storage_image',
+    ],
+    "data": [
+
+    ],
+    "demo": [
+    ],
+}
