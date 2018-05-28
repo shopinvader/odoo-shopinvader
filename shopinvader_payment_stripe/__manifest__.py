fbbc3c6875b1fd591863f2dd4dbb0c28f4761586
Sébastien BEAU <sebastien.beau@akretion.com>
2018-05-28 10:48:16 +0200

    [REF] rename module from shopinvader_stripe to shopinvader_payment_stripe

--- shopinvader_payment_stripe/__manifest__.py
+++ shopinvader_payment_stripe/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Shopinvader Stripe",
+    "name": "Shopinvader Payment Stripe",
     "summary": "Shopinvader Stripe Payment Gateway",
     "version": "10.0.1.0.0",
     "category": "e-commerce",
