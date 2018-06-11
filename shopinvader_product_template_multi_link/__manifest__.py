6b356cb6d9d3f0ad630919b038533e5cf13e158e
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-06-11 15:18:35 +0200

    [PORT]  shopinvader_product_template_multi_link [REF] rename module (old name in V8 shopinvader_product_link) [ADD] Test [IMP] only return the main 'shopinvader.variant' of each bind

--- shopinvader_product_template_multi_link/__manifest__.py
+++ shopinvader_product_template_multi_link/__manifest__.py
@@ -1,32 +1,25 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 # Copyright 2016 Akretion (http://www.akretion.com)
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # Sébastien BEAU <sebastien.beau@akretion.com>
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Shopinvader Product Link",
     "summary": "Shopinvader Product Link",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "e-commerce",
     "website": "https://akretion.com",
     "author": "Akretion",
     "license": "AGPL-3",
-    "application": False,
     "installable": True,
     "auto_install": True,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
     "depends": [
         "shopinvader",
-        "product_links",
+        "product_template_multi_link",
     ],
     "data": [
-        "data/ir_export.xml",
+        "data/ir_exports_line.xml",
     ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
 }
