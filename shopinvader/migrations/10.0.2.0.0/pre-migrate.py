fad1b84f2658924ee3bedc1462c976f883111f6b
Sébastien BEAU <sebastien.beau@akretion.com>
2018-06-12 13:06:21 +0200

    [FIX] fix migration and view of shopinvader backend

--- shopinvader/migrations/10.0.2.0.0/pre-migrate.py
+++ shopinvader/migrations/10.0.2.0.0/pre-migrate.py
@@ -7,7 +7,14 @@ from openupgradelib import openupgrade
 
 def migrate(cr, version):
     """Update database from previous versions, after updating module."""
+    openupgrade.rename_tables(
+        cr=cr,
+        table_spec=[('locomotive_backend', 'shopinvader_backend')]
+    )
     openupgrade.rename_models(
         cr=cr,
         model_spec=[('locomotive.backend', 'shopinvader.backend')]
     )
+    # remove blocking view
+    cr.execute("""DELETE FROM ir_ui_view
+        WHERE arch_db ilike '%locomotive_backend_ids%'""")
