af4efe35de6ef372a81e59b4b401ec5191398121
Benoit <benoit.guillot@akretion.com>
2018-07-31 10:41:35 +0200

    [IMP] add shopinvader state for quotation

--- shopinvader_quotation/models/sale.py
+++ shopinvader_quotation/models/sale.py
@@ -10,3 +10,14 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     typology = fields.Selection(selection_add=[('quotation', 'Quotation')])
+    shopinvader_state = fields.Selection(selection_add=[
+        ('estimating', 'Estimating Quotation'),
+        ('estimated', 'Estimated Quotation')])
+
+    def _get_shopinvader_state(self):
+        self.ensure_one()
+        if self.typology == 'quotation' and self.state == 'draft':
+            return 'estimating'
+        if self.typology == 'quotation' and self.state == 'draft':
+            return 'estimated'
+        return super(SaleOrder, self)._get_shopinvader_state()
