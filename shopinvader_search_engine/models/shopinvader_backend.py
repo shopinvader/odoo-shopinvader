4cc601781fad9b5bee3f5ec8dd7826c3a324c6f5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-08-22 15:52:46 +0200

    [IMP] shopinvader_search_engine: Ease developper work by allowing to build, clear and export search engine indexes all at once from the shopinvader backend

--- shopinvader_search_engine/models/shopinvader_backend.py
+++ shopinvader_search_engine/models/shopinvader_backend.py
@@ -3,7 +3,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ShopinvaderBackend(models.Model):
@@ -12,3 +12,20 @@ class ShopinvaderBackend(models.Model):
     se_backend_id = fields.Many2one(
         'se.backend',
         'Search Engine Backend')
+
+    @api.multi
+    def force_recompute_all_binding_index(self):
+        self.mapped('se_backend_id.index_ids').force_recompute_all_binding()
+        return True
+
+    @api.multi
+    def force_batch_export_index(self):
+        for index in self.mapped('se_backend_id.index_ids'):
+            index.force_batch_export()
+        return True
+
+    @api.multi
+    def clear_index(self):
+        for index in self.mapped('se_backend_id.index_ids'):
+            index.clear_index()
+        return True
