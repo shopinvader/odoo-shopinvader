7148d2290c1a4c4382e3dff653db2a7ae5726d49
Benoit <benoit.guillot@akretion.com>
2018-09-04 11:29:57 +0200

    [FIX] remove useless import

--- shopinvader_quotation/models/product.py
+++ shopinvader_quotation/models/product.py
@@ -3,7 +3,7 @@
 # Benoît GUILLOT <benoit.guillot@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
