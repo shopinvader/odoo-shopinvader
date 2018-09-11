59054bde505da541d53c776942e5c84813563f23
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-09-11 10:45:26 +0200

    [FIX] shopinvader: Take into account the fiscal position and the digital precision when computing the applied discount ant the original price for a product with a pricelist requesting to display the product with and without discount

--- shopinvader/models/shopinvader_variant.py
+++ shopinvader/models/shopinvader_variant.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.tools import float_round
 from .tools import sanitize_attr_name
 
 
@@ -130,6 +131,7 @@ class ShopinvaderVariant(models.Model):
         product_id = self.record_id
         taxes = product_id.taxes_id.sudo().filtered(
             lambda r: not company or r.company_id == company)
+        # get the expeced tax to apply from the fiscal position
         tax_id = fposition.map_tax(taxes, product_id) if fposition else taxes
         tax_id = tax_id and tax_id[0]
         product = product_id.with_context(
@@ -140,15 +142,26 @@ class ShopinvaderVariant(models.Model):
         final_price, rule_id = pricelist.get_product_price_rule(
             product, qty or 1.0, None)
         tax_included = tax_id.price_include
-        value = self.env['account.tax']._fix_tax_included_price_company(
+        account_tax_obj = self.env['account.tax']
+        # fix tax on the price
+        value = account_tax_obj._fix_tax_included_price_company(
             final_price, product.taxes_id, tax_id, company)
         res = {
             'value': value,
             'tax_included': tax_included
         }
         if pricelist.discount_policy == 'without_discount':
-            new_list_price, currency_id = self.env['sale.order.line']._get_real_price_currency(product, rule_id, qty or 1.0, product.uom_id, pricelist.id)
+            sol = self.env['sale.order.line']
+            new_list_price, currency_id = sol._get_real_price_currency(
+                product, rule_id, qty or 1.0, product.uom_id, pricelist.id)
+            # fix tax on the real price
+            new_list_price = account_tax_obj._fix_tax_included_price_company(
+                new_list_price, product.taxes_id, tax_id, company)
+
             discount = (new_list_price - value) / new_list_price * 100
+            # apply the right precision on discount
+            precision = self.env['decimal.precision'].precision_get('Discount')
+            discount = float_round(discount, precision)
             res.update({
                 'original_value': new_list_price,
                 'discount': discount,
