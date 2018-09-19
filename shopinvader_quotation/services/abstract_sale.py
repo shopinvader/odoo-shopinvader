6406689902f9f4c8430c182b0630ba9dff426d59
Benoit <benoit.guillot@akretion.com>
2018-09-19 12:22:38 +0200

    [IMP] add only_quotation in cart product info

---
+++ shopinvader_quotation/services/abstract_sale.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Akretion (http://www.akretion.com)
+# Benoît GUILLOT <benoit.guillot@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.addons.component.core import AbstractComponent
+
+
+class AbstractSaleService(AbstractComponent):
+    _inherit = 'shopinvader.abstract.sale.service'
+
+    def _parser_product(self):
+        res = super(AbstractSaleService, self)._parser_product()
+        res.append('only_quotation')
+        return res
