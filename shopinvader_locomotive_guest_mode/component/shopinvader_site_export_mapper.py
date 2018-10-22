40bc9fb6a9cae444df9d9362f5291384a3c080bd
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-10-22 17:34:18 +0200

    [ADD] shopinvader_locomotive_guest_mode: Synchronize is_guest_mode_amowed into the site metadata

---
+++ shopinvader_locomotive_guest_mode/component/shopinvader_site_export_mapper.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class ShopinvaderSiteExportMapper(Component):
+    _inherit = 'shopinvader.site.export.mapper'
+
+    direct = [
+        ('is_guest_mode_allowed', 'is_guest_mode_allowed')
+    ]
