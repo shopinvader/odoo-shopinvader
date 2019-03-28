a0359b770912c89e8a238d6fbe9fb5fd913dedda
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-03-28 10:28:08 +0100

    [ADD] shopinvader-elasticsearch: Shopinvader Elasticsearch Connector

---
+++ shopinvader_elasticsearch/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Elasticsearch",
+    "description": """
+        Shopinvader Elasticsearch Connector""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": ["shopinvader_search_engine", "connector_elasticsearch"],
+    "data": ["data/ir_export_product.xml"],
+    "demo": [],
+}
