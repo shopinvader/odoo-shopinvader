624dd37bf2a5e131ebb4054c5620ea320af93a6a
François Honoré <francois.honore@acsone.eu>
2019-04-03 15:34:38 +0200

    [ADD][10.0] Add shopinvader_sale_report module

---
+++ shopinvader_sale_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Shopinvader sale report",
+    'description': """Shopinvader addons to extend sale report with backend""",
+    'author': 'ACSONE SA/NV',
+    'website': "http://acsone.eu",
+    'category': 'e-commerce',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'shopinvader',
+    ],
+}
