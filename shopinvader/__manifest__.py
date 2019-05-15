b7803b35a97990acd76d158a5de4cc977a19ce27
François Honoré <francois.honore@acsone.eu>
2019-05-15 12:30:59 +0200

    [10.0][ADD] Add wizard to bind shopinvader.partner (same behaviour like partner portal access rights)

--- shopinvader/__manifest__.py
+++ shopinvader/__manifest__.py
@@ -43,6 +43,7 @@
         "wizards/shopinvader_variant_binding_wizard.xml",
         "wizards/shopinvader_category_binding_wizard.xml",
         "wizards/shopinvader_category_unbinding_wizard.xml",
+        "wizards/shopinvader_partner_binding.xml",
         "views/shopinvader_menu.xml",
         "views/shopinvader_product_view.xml",
         "views/shopinvader_variant_view.xml",
