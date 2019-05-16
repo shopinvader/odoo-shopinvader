fa81c9e11bef41857d8d9b211218bb44973b13a3
François Honoré <francois.honore@acsone.eu>
2019-05-16 11:29:45 +0200

    [10.0][ADD] Add shopinvader_invoice

---
+++ shopinvader_invoice/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Shopinvader Invoice",
+    "description": """Shopinvader Invoice module""",
+    "author": "ACSONE SA/NV",
+    "website": "https://shopinvader.com",
+    "category": "shopinvader",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account", "shopinvader"],
+    "data": [],
+    "demo": [],
+}
