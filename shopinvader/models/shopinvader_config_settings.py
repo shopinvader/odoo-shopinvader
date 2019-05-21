af12df29fcc2a9392a317df7571c7422b797cba4
Denis Roussel <denis.roussel@acsone.eu>
2019-05-21 11:14:06 +0200

    [12.0][FIX] shopinvader: Change config settings

--- shopinvader/models/shopinvader_config_settings.py
+++ shopinvader/models/shopinvader_config_settings.py
@@ -3,36 +3,20 @@
 
 from odoo import api, fields, models
 
-PARAMS = [("no_partner_duplicate", "shopinvader.no_partner_duplicate")]
-
 
 class ShopinvaderConfigSettings(models.TransientModel):
+
     _inherit = "res.config.settings"
     _name = "shopinvader.config.settings"
 
     no_partner_duplicate = fields.Boolean(
         default=True,
+        config_parameter="shopinvader.no_partner_duplicate",
         help="If checked, when a binding is created for a backend, we first "
         "try to find a partner with the same email and if found we link "
         "the new binding to the first partner found. Otherwise we always "
         "create a new partner",
     )
-
-    @api.multi
-    def set_params(self):
-        self.ensure_one()
-        for field_name, key_name in PARAMS:
-            value = getattr(self, field_name, "")
-            self.env["ir.config_parameter"].set_param(key_name, value)
-
-    @api.model
-    def get_default_params(self, fields):
-        res = {}
-        for field_name, key_name in PARAMS:
-            res[field_name] = (
-                self.env["ir.config_parameter"].get_param(key_name, "").strip()
-            )
-        return res
 
     # external methods
     @api.model
