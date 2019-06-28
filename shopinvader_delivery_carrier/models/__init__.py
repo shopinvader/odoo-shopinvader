886dd355fb8846f48be2ddf0ef3c0d3e16981f22
François Honoré <francois.honore@acsone.eu>
2019-06-28 14:20:59 +0200

    [ADD][10.0] add delivery price simulation for cart

--- shopinvader_delivery_carrier/models/__init__.py
+++ shopinvader_delivery_carrier/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import backend
+from . import delivery_carrier
 from . import sale
 from . import shopinvader_notification
 from . import stock_picking
--- shopinvader_delivery_carrier/services/cart.py
+++ shopinvader_delivery_carrier/services/cart.py
@@ -3,6 +3,7 @@
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
@@ -11,14 +12,25 @@ from odoo.tools.translate import _
 class CartService(Component):
     _inherit = "shopinvader.cart.service"
 
-    def get_delivery_methods(self):
+    def get_delivery_methods(self, **params):
         """
-            This service will return all possible delivery methods for the
-            current cart
-        :return:
+        This service will return all possible delivery methods for the
+        current cart (depending on country/zip)
+        The cart is not updated with the given country/zip. The change is done
+        only in memory.
+        :param params: dict
+        :return: dict
         """
         cart = self._get()
-        return self._get_available_carrier(cart)
+        country = self._load_country(params)
+        zip_code = self._load_zip_code(params)
+        if country or zip_code:
+            cart = cart.with_context(
+                delivery_force_country_id=country.id,
+                delivery_force_zip_code=zip_code,
+            )
+        result = self._get_available_carrier(cart)
+        return result
 
     def apply_delivery_method(self, **params):
         """
@@ -51,9 +63,33 @@ class CartService(Component):
         }
 
     def _validator_get_delivery_methods(self):
-        return {}
+        return {
+            "country_id": {
+                "coerce": to_int,
+                "required": False,
+                "type": "integer",
+            },
+            "zip_code": {"required": False, "type": "string"},
+        }
 
     # internal methods
+    def _load_country(self, params):
+        """
+        Load the country from given params
+        :param params: dict
+        :return: res.country recordset
+        """
+        country_id = params.pop("country_id", 0)
+        return self.env["res.country"].browse(country_id)
+
+    def _load_zip_code(self, params):
+        """
+        Load the country from given params
+        :param params: dict
+        :return: str
+        """
+        return params.pop("zip_code", "")
+
     def _add_item(self, cart, params):
         res = super(CartService, self)._add_item(cart, params)
         self._unset_carrier(cart)
