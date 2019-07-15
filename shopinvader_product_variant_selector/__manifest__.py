f3bab8965ffbbbc6a9b675ef7853582d9639191f
Sébastien BEAU <sebastien.beau@akretion.com>
2019-07-15 15:50:33 +0200

    [ADD] add module shopinvader_product_selector

---
+++ shopinvader_product_variant_selector/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Product Variant Selector",
+    "summary": "Module that allow multi selector on shopinvader site",
+    "version": "10.0.1.0.0",
+    "category": "Shopinvader",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["shopinvader"],
+    "data": ["data/ir_export_product.xml"],
+    "demo": [],
+    "qweb": [],
+}
