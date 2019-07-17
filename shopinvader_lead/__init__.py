89b615735f36a9b73a0004ce2d3f475f992e1068
Denis Roussel <denis.roussel@acsone.eu>
2019-07-17 08:26:42 +0200

    [12.0][MIG] shopinvader_lead

--- shopinvader_lead/__init__.py
+++ shopinvader_lead/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from . import controllers
 from . import services
 from . import models
--- shopinvader_lead/__manifest__.py
+++ shopinvader_lead/__manifest__.py
@@ -1,15 +1,13 @@
-# -*- coding: utf-8 -*-
-
 {
     "name": "Shopinvader Lead",
     "summary": "Shopinvader Lead Management",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "e-commerce",
-    "website": "https://shopinvader.com",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "depends": ["shopinvader", "crm"],
     "demo": ["demo/email_demo.xml", "demo/notification_demo.xml"],
 }
