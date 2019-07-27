9a54fc4b0016c00217c879675752588cca799234
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-07-27 15:20:12 +0200

    [12.0][MIG] shopinvader_backend_image_proxy

--- shopinvader_backend_image_proxy/__manifest__.py
+++ shopinvader_backend_image_proxy/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion (http://www.akretion.com).
 # Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,12 +5,12 @@
     "name": "Shopinvader backend image proxy",
     "summary": "Add possibility to replace the image URL by the proxy url "
     "set on the SE backend",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "e-commerce",
     "author": "Akretion,ACSONE SA/NV",
     "website": "http://www.akretion.com",
     "license": "AGPL-3",
     "depends": ["shopinvader_image"],
     "data": ["views/shopinvader_backend.xml"],
-    "installable": False,
+    "installable": True,
 }
