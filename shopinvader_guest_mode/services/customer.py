803e9a8018c7d5bc5c71d7a26ac25f6bfc759c83
Cédric Pigeon <cedric.pigeon@acsone.eu>
2019-09-04 09:50:38 +0200

    [FIX] fix error message when a guest tries to create an account before the expiry delay

---
+++ shopinvader_guest_mode/services/customer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class CustomerService(Component):
+    _inherit = "shopinvader.customer.service"
+    _usage = "customer"
+
+    def create(self, **params):
+        guest = self.component(usage="guest")
+        guest._archive_existing_binding(email=params["email"])
+        return super(CustomerService, self).create(**params)
