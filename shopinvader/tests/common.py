a939ca01c12b2a838770b3cb12c91e8247e50022
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-09-06 18:26:57 +0200

    [IMP] Replace shopinvader_session_data by ShopinvaderSResponse this the unique usage of this concept is to enrich the response returned by a service

--- shopinvader/tests/common.py
+++ shopinvader/tests/common.py
@@ -12,7 +12,7 @@ from odoo.addons.component.core import WorkContext
 from odoo.addons.component.tests.common import ComponentMixin
 from odoo.addons.queue_job.job import Job
 from odoo.addons.server_environment import serv_config
-from odoo.addons.shopinvader import session
+from odoo.addons.shopinvader import shopinvader_response
 from odoo.tests import SavepointCase
 
 
@@ -69,11 +69,11 @@ class CommonCase(SavepointCase, ComponentMixin):
         SavepointCase.setUp(self)
         ComponentMixin.setUp(self)
 
-        session.set_testmode(True)
+        shopinvader_response.set_testmode(True)
 
         @self.addCleanup
-        def cleanupSessionTestMode():
-            session.set_testmode(False)
+        def cleanupShopinvaderResponseTestMode():
+            shopinvader_response.set_testmode(False)
 
 
 class ProductCommonCase(CommonCase):
