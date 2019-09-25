7db99a94fe4db64a86f6d18c856b642619c63025
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-09-25 18:04:04 +0200

    [IMP] shopinvader_locomotive: Add new button to easily configure locomotive from informations managed by odoo (api key, odoo url, index names, ....)

---
+++ shopinvader_locomotive_elasticsearch/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Locomotive Elasticsearch",
+    "description": """
+        This addons is used to push the initial elasticsearch configuration
+        to locomotive""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "https://acsone.eu/",
+    "depends": [
+        "component",
+        "shopinvader_locomotive",
+        "shopinvader_elasticsearch",
+    ],
+}
