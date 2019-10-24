50647e79d6a221253b55638589238fae7e49c3f2
Simone Orsi <simahawk@gmail.com>
2019-10-24 11:04:12 +0200

    [12.0] Add shopinvader_partner_firstname

--- shopinvader/tests/test_customer.py
+++ shopinvader/tests/test_customer.py
@@ -9,9 +9,9 @@ from .common import CommonCase
 _logger = logging.getLogger(__name__)
 
 
-class TestCustomer(CommonCase):
+class TestCustomerCommon(CommonCase):
     def setUp(self, *args, **kwargs):
-        super(TestCustomer, self).setUp(*args, **kwargs)
+        super(TestCustomerCommon, self).setUp(*args, **kwargs)
         self.data = {
             "email": "new@customer.example.com",
             "name": "Purple",
@@ -27,6 +27,17 @@ class TestCustomer(CommonCase):
         ) as work:
             self.service = work.component(usage="customer")
 
+    def _test_partner_data(self, partner, data):
+        for key in data:
+            if key == "external_id":
+                continue
+            elif key == "country":
+                self.assertEqual(partner.country_id.id, data[key]["id"])
+            else:
+                self.assertEqual(partner[key], data[key])
+
+
+class TestCustomer(TestCustomerCommon):
     def test_create_customer(self):
         self.data["external_id"] = "D5CdkqOEL"
         res = self.service.dispatch("create", params=self.data)["data"]
@@ -35,13 +46,7 @@ class TestCustomer(CommonCase):
         self.assertEqual(
             partner.shopinvader_bind_ids.external_id, self.data["external_id"]
         )
-        for key in self.data:
-            if key == "external_id":
-                continue
-            elif key == "country":
-                self.assertEqual(partner.country_id.id, self.data[key]["id"])
-            else:
-                self.assertEqual(partner[key], self.data[key])
+        self._test_partner_data(partner, self.data)
 
     def test_create_customer_business_vat_only(self):
         self.data["external_id"] = "D5CdkqOEL"
