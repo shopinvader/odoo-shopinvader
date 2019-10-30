63ba605df647d4b212bdac070501f5ca00b12c19
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-10-30 10:34:41 +0100

    [MIG] shopinvader_cart_expiry: Migration to 12.0

--- shopinvader_cart_expiry/__manifest__.py
+++ shopinvader_cart_expiry/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -7,7 +6,7 @@
     "author": "ACSONE SA/NV",
     "website": "http://www.shopinvader.com",
     "category": "e-commerce",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["shopinvader", "queue_job"],
     "data": ["data/ir_cron.xml", "views/shopinvader_backend.xml"],
