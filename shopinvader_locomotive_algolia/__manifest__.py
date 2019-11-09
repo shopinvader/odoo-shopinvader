b98923382a823cce00718f2449fd5c9c92217379
Simone Orsi <simahawk@gmail.com>
2019-11-09 11:48:38 +0100

    [12.0] Add shopinvader_locomotive_algolia

---
+++ shopinvader_locomotive_algolia/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Locomotive algolia",
+    "summary": """
+        This addons is used to push the initial algolia configuration
+        to locomotive""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptcamp SA",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["component", "shopinvader_locomotive", "shopinvader_algolia"],
+}
---
+++ shopinvader_locomotive_algolia/component/shopinvader_site_export_mapper.py
@@ -0,0 +1,25 @@
+# Copyright 2019 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class ShopinvaderSiteExportMapper(Component):
+    _inherit = ["shopinvader.site.export.mapper"]
+
+    # matching $searchengine_name + '_config'
+    def _algolia_config(self, se_backend, config):
+        if (
+            self.options["force"]
+            or not config.get("application_id")
+            or not config.get("api_key")
+        ):
+            spec_backend = se_backend.specific_backend
+            config.update(
+                {
+                    "application_id": spec_backend.algolia_app_id,
+                    "api_key": spec_backend.algolia_api_key,
+                }
+            )
+        return config
