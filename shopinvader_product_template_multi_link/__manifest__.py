a1f90cecb6d8e8bf80668d95c675b495481c4a88
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-15 16:31:05 +0100

    [MIG] shopinvader_product_template_multi_link: Migration to 13.0

--- shopinvader_product_template_multi_link/__manifest__.py
+++ shopinvader_product_template_multi_link/__manifest__.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright 2016 Akretion (http://www.akretion.com)
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # Sébastien BEAU <sebastien.beau@akretion.com>
@@ -8,12 +7,12 @@
 {
     "name": "Shopinvader Product Link",
     "summary": "Shopinvader Product Link",
-    "version": "10.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "e-commerce",
     "website": "https://akretion.com",
     "author": "Akretion,GRAP",
     "license": "AGPL-3",
-    "installable": False,
+    "installable": True,
     "auto_install": True,
     "depends": ["shopinvader", "product_template_multi_link"],
     "data": ["data/ir_exports_line.xml"],
--- shopinvader_product_template_multi_link/tests/test_product.py
+++ shopinvader_product_template_multi_link/tests/test_product.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,12 +6,49 @@ from odoo.addons.shopinvader.tests.common import ProductCommonCase
 
 
 class ProductCase(ProductCommonCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.template_1 = cls.env.ref(
+            "product.product_product_7_product_template"
+        )
+        cls.template_2 = cls.env.ref(
+            "product.product_product_9_product_template"
+        )
+        cls.template_1.product_template_link_ids.unlink()
+        cls.up_selling_link = cls.env["product.template.link"].create(
+            {
+                "left_product_tmpl_id": cls.template_1.id,
+                "right_product_tmpl_id": cls.template_2.id,
+                "type_id": cls.env.ref(
+                    "product_template_multi_link."
+                    "product_template_link_type_up_selling"
+                ).id,
+            }
+        )
+        cls.cross_selling_link = cls.env["product.template.link"].create(
+            {
+                "left_product_tmpl_id": cls.template_1.id,
+                "right_product_tmpl_id": cls.template_2.id,
+                "type_id": cls.env.ref(
+                    "product_template_multi_link."
+                    "product_template_link_type_cross_selling"
+                ).id,
+            }
+        )
+
     def test_cross_sellings_link(self):
-        self.backend.bind_all_category()
-        template_1 = self.env.ref("product.product_product_7_product_template")
-        template_2 = self.env.ref("product.product_product_9_product_template")
-        res = template_1.shopinvader_bind_ids[0].cross_selling_ids
+        res = self.template_1.shopinvader_bind_ids[0].cross_selling_ids
         self.assertEqual(len(res), 1)
         self.assertEqual(
-            res, template_2.shopinvader_bind_ids[0].shopinvader_variant_ids
+            res,
+            self.template_2.shopinvader_bind_ids[0].shopinvader_variant_ids,
+        )
+
+    def test_up_sellings_link(self):
+        res = self.template_1.shopinvader_bind_ids[0].up_selling_ids
+        self.assertEqual(len(res), 1)
+        self.assertEqual(
+            res,
+            self.template_2.shopinvader_bind_ids[0].shopinvader_variant_ids,
         )
