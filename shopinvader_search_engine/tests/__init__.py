4ff70f78f07e92c94a2c93cde6e5b68a7465a9dc
François Honoré <francois.honore@acsone.eu>
2019-11-18 09:06:21 +0100

    Use the technical user to recompute and export (buttons on shopinvader backend view) [ADD] Add unit tests

--- shopinvader_search_engine/tests/__init__.py
+++ shopinvader_search_engine/tests/__init__.py
@@ -1,1 +1,3 @@
-from . import test_delete_product
+# Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from . import test_shopinvader_backend
