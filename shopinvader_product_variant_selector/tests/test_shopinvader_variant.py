f95d5f2fa8e520da59b44e43f62065b7db6183b3
beau sebastien <sebastien.beau@akretion.com>
2019-11-26 15:25:17 +0100

    Apply suggestions from code review
    Co-Authored-By: Simone Orsi <simahawk@users.noreply.github.com>

--- shopinvader_product_variant_selector/tests/test_shopinvader_variant.py
+++ shopinvader_product_variant_selector/tests/test_shopinvader_variant.py
@@ -252,9 +252,9 @@ class ShopinvaderVariantCase(SavepointCase):
 
     def test_selector_with_three_dimension_with_many_inactive(self):
         variant = self._configure_and_get_variant("Poster-White-70x50cm")
-        # We inactive all Wooden product than match the White and 70x50cm value
+        # We make inactive all Wooden product than match the White and 70x50cm value
         # in order to force the system to propose for the selector Frame Type
-        # a variant that do not match this 2 values
+        # a variant that does not match these 2 values
         self._inactive_variant(
             [
                 "Poster-White-45x30cm",
