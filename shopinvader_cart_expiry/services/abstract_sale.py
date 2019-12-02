63e5b5304e2004e2159fbf5980d3ad5971138533
Denis Roussel <denis.roussel@acsone.eu>
2019-12-02 12:47:45 +0100

    [12.0][IMP] shopinvader_cart_expiry: Add expiration unit
    Improvement to take into account some particular usages of
    cart expiration delays.
    
    The service that retrieves cart, includes now the expiration date.

---
+++ shopinvader_cart_expiry/services/abstract_sale.py
@@ -0,0 +1,15 @@
+# Copyright 2019 ACSONE SA/NV https://acsone.eu
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.addons.component.core import AbstractComponent
+
+
+class AbstractSaleService(AbstractComponent):
+
+    _inherit = "shopinvader.abstract.sale.service"
+
+    def _convert_one_sale(self, sale):
+        res = super(AbstractSaleService, self)._convert_one_sale(sale)
+        res.update({"expiration_date": sale.cart_expiration_date})
+
+        return res
