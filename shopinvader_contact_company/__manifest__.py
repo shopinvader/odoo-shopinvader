823b9350d893858d78d3d53f091f732263e92575
Simone Orsi <simahawk@gmail.com>
2020-01-08 15:10:50 +0100

    [MIG] shopinvader_contact_company: Migration to 13.0

--- shopinvader_contact_company/__manifest__.py
+++ shopinvader_contact_company/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com)
 # Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,13 +5,13 @@
 {
     "name": "Shopinvader Contact Company",
     "summary": "Make available the field company in the address form",
-    "version": "10.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "e-commerce",
     "website": "https://akretion.com",
     "author": "Akretion",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "auto_install": True,
     "external_dependencies": {"python": [], "bin": []},
     "depends": ["shopinvader", "partner_contact_company"],
