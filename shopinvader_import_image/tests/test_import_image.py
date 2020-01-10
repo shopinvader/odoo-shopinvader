0a9317d457e1521d18edca5c5e97f158b16b781a
Simone Orsi <simahawk@gmail.com>
2020-01-10 08:54:05 +0100

    shopinvader_import_image: refactoring

--- shopinvader_import_image/tests/test_import_image.py
+++ shopinvader_import_image/tests/test_import_image.py
@@ -1,60 +1,145 @@
-# Copyright 2017 Akretion (http://www.akretion.com).
-# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2020 Camptocamp (http://www.camptocamp.com)
+# @author Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import base64
+import os
 
-from odoo.addons.shopinvader.tests.common import ProductCommonCase
-from odoo.addons.storage_image_product.tests.common import (
-    ProductImageCommonCase,
-)
+from odoo.addons.shopinvader_image.tests.common import TestShopinvaderImageCase
 
 
-class ShopinvaderImageCase(ProductCommonCase, ProductImageCommonCase):
-    def setUp(self):
-        super(ShopinvaderImageCase, self).setUp()
-        self.env.ref("base.user_demo").write(
-            {
-                "groups_id": [
-                    (
-                        4,
-                        self.env.ref(
-                            "shopinvader.group_shopinvader_manager"
-                        ).id,
-                    )
-                ]
-            }
+class TestShopinvaderImportImageCase(TestShopinvaderImageCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.storage_backend = cls.env.ref(
+            "storage_backend.default_storage_backend"
         )
-        ProductImageCommonCase.setUp(self)
-        self.logo = self.env["product.image.relation"].create(
-            {
-                "product_tmpl_id": self.template.id,
-                "image_id": self.logo_image.id,
-            }
+        cls.base_path = os.path.dirname(os.path.abspath(__file__))
+        cls.file_csv_content = cls._get_file_content(
+            "image_import_test.csv", base_path=cls.base_path
         )
-        self.image_bk = self.env["product.image.relation"].create(
-            {
-                "product_tmpl_id": self.template.id,
-                "image_id": self.black_image.id,
-                "attribute_value_ids": [
-                    (
-                        6,
-                        0,
-                        [self.env.ref("product.product_attribute_value_4").id],
-                    )
-                ],
-            }
+        cls.file_zip_content = cls._get_file_content(
+            "image_import_test.zip", base_path=cls.base_path
         )
 
-    def test_getting_image_for_black_product(self):
-        images = self.shopinvader_variant.images
-        self.assertEqual(len(images), 2)
-        for image in images:
-            self.assertIn("small", image)
-            self.assertIn("medium", image)
-            self.assertIn("large", image)
+        cls.wiz = cls._get_wizard(cls)
+        cls.products = cls.env["product.template"].search([], limit=2)
+        cls.products[0].write({"default_code": "A001", "image_ids": False})
+        cls.products[1].write({"default_code": "A002", "image_ids": False})
 
-        # check url key of image that shoul match product name slugified
-        for image in self.shopinvader_variant.image_ids.mapped("image_id"):
-            # skip the two first thumbnail as there are odoo thumbnail
-            for thumbnail in image.thumbnail_ids[2:]:
-                self.assertEqual(thumbnail.url_key, "customizable-desk-config")
+    def _get_wizard(self, **kw):
+        vals = {
+            "storage_backend_id": self.storage_backend.id,
+            "product_model": "product.template",
+            "file_csv": self.file_csv_content,
+            "file_zip": self.file_zip_content,
+        }
+        vals.update(kw)
+        return self.env["shopinvader.import.product_image"].create(vals)
+
+
+class TestShopinvaderImportImage(TestShopinvaderImportImageCase):
+    def test_get_lines(self):
+        lines = self.wiz._get_lines()
+        expected = [
+            {
+                "default_code": "A00%d" % x,
+                "tag_name": "A00%d tag" % x,
+                "file_path": "A00%d.jpg" % x,
+            }
+            for x in range(1, 4)
+        ]
+        self.assertEqual(lines, expected)
+
+    def test_read_from_zip(self):
+        img_content = self._get_file_content(
+            "A001.jpg", base_path=self.base_path, as_binary=True
+        )
+        self.assertEqual(self.wiz._read_from_zip("A001.jpg"), img_content)
+
+    def test_get_b64(self):
+        img_content = self._get_file_content(
+            "A001.jpg", base_path=self.base_path, as_binary=True
+        )
+        self.assertEqual(
+            self.wiz._get_base64("A001.jpg"),
+            ("image/jpeg", base64.encodestring(img_content)),
+        )
+
+    def test_import_errors(self):
+        self.products[0].default_code = "NONE"
+        self.products[1].default_code = "NONE"
+        self.wiz.do_import()
+        self.assertEqual(
+            self.wiz.report,
+            {
+                "created": [],
+                "missing": ["A001", "A002", "A003"],
+                "missing_tags": ["A001 tag", "A002 tag", "A003 tag"],
+            },
+        )
+
+    def test_import_no_overwrite(self):
+        self.wiz.do_import()
+        self.assertEqual(
+            self.wiz.report,
+            {
+                "created": ["A001", "A002"],
+                "missing": ["A003"],
+                "missing_tags": ["A001 tag", "A002 tag", "A003 tag"],
+            },
+        )
+        self.assertEqual(len(self.products[0].image_ids), 1)
+        self.assertEqual(len(self.products[1].image_ids), 1)
+        self.assertFalse(self.products[0].image_ids[0].tag_id)
+        self.assertFalse(self.products[1].image_ids[0].tag_id)
+
+        self.wiz.do_import()
+        self.assertEqual(
+            self.wiz.report,
+            {
+                "created": ["A001", "A002"],
+                "missing": ["A003"],
+                "missing_tags": ["A001 tag", "A002 tag", "A003 tag"],
+            },
+        )
+        self.assertEqual(len(self.products[0].image_ids), 2)
+        self.assertEqual(len(self.products[1].image_ids), 2)
+
+    def test_import_overwrite(self):
+        self.wiz.overwrite = True
+        self.wiz.do_import()
+        self.assertEqual(
+            self.wiz.report,
+            {
+                "created": ["A001", "A002"],
+                "missing": ["A003"],
+                "missing_tags": ["A001 tag", "A002 tag", "A003 tag"],
+            },
+        )
+        self.assertEqual(len(self.products[0].image_ids), 1)
+        self.assertEqual(len(self.products[1].image_ids), 1)
+        self.wiz.do_import()
+        self.assertEqual(
+            self.wiz.report,
+            {
+                "created": ["A001", "A002"],
+                "missing": ["A003"],
+                "missing_tags": ["A001 tag", "A002 tag", "A003 tag"],
+            },
+        )
+        self.assertEqual(len(self.products[0].image_ids), 1)
+        self.assertEqual(len(self.products[1].image_ids), 1)
+
+    def test_import_create_missing_tags(self):
+        self.wiz.overwrite = True
+        self.wiz.create_missing_tags = True
+        self.wiz.do_import()
+        self.assertEqual(
+            self.wiz.report, {"created": ["A001", "A002"], "missing": ["A003"]}
+        )
+        self.assertEqual(len(self.products[0].image_ids), 1)
+        self.assertEqual(len(self.products[1].image_ids), 1)
+        self.assertEqual(self.products[0].image_ids[0].tag_id.name, "A001 tag")
+        self.assertEqual(self.products[1].image_ids[0].tag_id.name, "A002 tag")
