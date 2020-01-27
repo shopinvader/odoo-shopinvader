fe41752a9d5ed7856d78120d8ba05e6d60545330
Alexis de Lattre <alexis.delattre@akretion.com>
2020-01-27 09:52:51 +0100

    shopinvader.partner: switch to ondelete=restrict

--- shopinvader/models/shopinvader_partner.py
+++ shopinvader/models/shopinvader_partner.py
@@ -14,7 +14,7 @@ class ShopinvaderPartner(models.Model):
     _inherits = {"res.partner": "record_id"}
 
     record_id = fields.Many2one(
-        "res.partner", required=True, ondelete="cascade"
+        "res.partner", required=True, ondelete="restrict"
     )
     partner_email = fields.Char(
         related="record_id.email", readonly=True, required=True, store=True
