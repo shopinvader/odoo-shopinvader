9e75fc5de0ec7cee7f331954eb510ab64ac1c446
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-03-13 11:41:31 +0100

    13.0 Migrate shopinvader_lead

--- shopinvader_lead/__manifest__.py
+++ shopinvader_lead/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "Shopinvader Lead",
     "summary": "Shopinvader Lead Management",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "depends": ["shopinvader", "crm"],
     "demo": ["demo/email_demo.xml", "demo/notification_demo.xml"],
 }
