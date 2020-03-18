0c96501088e11f1fb4ecf2feb51a0c514ed5863b
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-03-18 19:03:35 +0100

    [FIX] add missing translation

--- shopinvader/controllers/main.py
+++ shopinvader/controllers/main.py
@@ -6,6 +6,7 @@
 
 import logging
 
+from odoo import _
 from odoo.addons.base_rest.controllers import main
 from odoo.exceptions import MissingError
 from odoo.http import request, route
@@ -47,7 +48,7 @@ class InvaderController(main.RestController):
                     )
                 # Could be because the email is not related to a partner or
                 # because the partner is inactive
-                raise MissingError("The given partner is not found!")
+                raise MissingError(_("The given partner is not found!"))
         return partner_model.browse([]).record_id
 
     @classmethod
