51a60c7d8c6136e9ee6745d89e7bc9bea90edb07
Pierrick Brun <pierrick.brun@akretion.com>
2020-03-31 14:26:42 +0200

    [FIX] encoding pragma

--- shopinvader_locomotive_elasticsearch/__manifest__.py
+++ shopinvader_locomotive_elasticsearch/__manifest__.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Shopinvader Locomotive Elasticsearch",
-    "description": """
+    "summary": """
         This addons is used to push the initial elasticsearch configuration
         to locomotive""",
     "version": "10.0.1.0.0",
