3f4407eb2367e126a0cda58b9f1dda86f2a5763c
Sébastien BEAU <sebastien.beau@akretion.com>
2020-04-03 22:33:21 +0200

    [FIX] fix issue when creating binding from the main record
    Indeed the implementation of write on one2many (see odoo/fields.py:2235)
    call the method create/write in mode norecompute
    This mean that the field is_urls_sync_required is not computed after
    the call to super of write/create of the binding but later during the
    create/write of the main record.
    
    To solve the issue we sync the url after calling the method
    "_recompute_done".
    This also simplify the code \o/

--- base_url/tests/models.py
+++ base_url/tests/models.py
@@ -18,6 +18,15 @@ class UrlBackendFake(models.Model, TestMixin):
     name = fields.Char(required=True)
 
 
+class ResPartner(models.Model, TestMixin):
+    _name = "res.partner"
+    _inherit = "res.partner"
+    _test_teardown_no_delete = True
+    _test_purge_fields = ["binding_ids"]
+
+    binding_ids = fields.One2many("res.partner.addressable.fake", "record_id")
+
+
 class ResPartnerAddressableFake(models.Model, TestMixin):
     _name = "res.partner.addressable.fake"
     _inherit = "abstract.url"
@@ -25,8 +34,15 @@ class ResPartnerAddressableFake(models.Model, TestMixin):
     _description = "Fake partner addressable"
 
     backend_id = fields.Many2one(comodel_name="url.backend.fake")
+    special_code = fields.Char()
 
     @api.multi
-    @api.depends("lang_id", "record_id.name")
+    @api.depends("lang_id", "special_code", "record_id.name")
     def _compute_automatic_url_key(self):
         self._generic_compute_automatic_url_key()
+
+    def _get_url_keywords(self):
+        res = super(ResPartnerAddressableFake, self)._get_url_keywords()
+        if self.special_code:
+            res += [self.special_code]
+        return res
