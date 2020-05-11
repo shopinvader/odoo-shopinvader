69cc1d616a35b1f5aaaa6cc4348af8b2d2aa7241
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-05-11 13:28:08 +0200

    [13.0][MIG] shopinvader_locomotive_guest_mode

--- shopinvader_locomotive_guest_mode/__manifest__.py
+++ shopinvader_locomotive_guest_mode/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Shopinvader Locomotive Guest Mode",
     "summary": """
         Shopinvader guest mode for locomotive""",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "development_status": "Stable/Production",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "depends": ["shopinvader_guest_mode", "shopinvader_locomotive"],
-    "installable": False,
+    "installable": True,
 }
