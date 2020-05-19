8a0dc5ab6c9ffb8b6fd9de0b36588509f3a07edd
Benoit <benoit.guillot@akretion.com>
2020-05-19 09:42:28 +0200

    [IMP] add tests for recompute action

--- shopinvader_search_engine/tests/__init__.py
+++ shopinvader_search_engine/tests/__init__.py
@@ -1,1 +1,2 @@
 from . import test_delete_product
+from . import test_action_server
---
+++ shopinvader_search_engine/tests/test_action_server.py
@@ -0,0 +1,37 @@
+# Copyright 2020 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.queue_job.tests.common import JobMixin
+from odoo.addons.shopinvader.tests.common import ProductCommonCase
+
+
+class ActionServerCase(ProductCommonCase, JobMixin):
+    def test_action_server_on_product_template(self):
+        job = self.job_counter()
+        # we take the number of variant linked => the number of created jobs
+        bindings = self.env["shopinvader.product"].search([], limit=4)
+        variant_length = len(bindings.mapped("shopinvader_variant_ids"))
+        action = self.env.ref(
+            "shopinvader_search_engine.action_recompute_shopinvader_product"
+        )
+        action_context = action.with_context(
+            active_model="product.template",
+            active_ids=bindings.mapped("record_id").ids,
+        )
+        action_context.run()
+        self.assertEqual(job.count_created(), variant_length)
+
+    def test_action_server_on_product_category(self):
+        self.backend.bind_all_category()
+        job = self.job_counter()
+        bindings = self.env["shopinvader.category"].search([], limit=4)
+        action = self.env.ref(
+            "shopinvader_search_engine.action_recompute_shopinvader_category"
+        )
+        action_context = action.with_context(
+            active_model="product.category",
+            active_ids=bindings.mapped("record_id").ids,
+        )
+        action_context.run()
+        self.assertEqual(job.count_created(), 4)
