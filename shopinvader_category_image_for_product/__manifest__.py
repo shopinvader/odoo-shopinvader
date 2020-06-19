029a3a0e42e689ad0b2a32b39b0c59de78559bdb
Sébastien BEAU <sebastien.beau@akretion.com>
2020-06-19 19:22:20 +0200

    [MIG] migrate to 12.0

--- shopinvader_category_image_for_product/__manifest__.py
+++ shopinvader_category_image_for_product/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Shopinvader category image for product",
     "summary": "Shopinvader Display category image for product",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "e-commerce",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
