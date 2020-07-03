1314805cade8256d05b8552d40706581c6d3917c
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-07-03 11:18:59 +0200

    Nest allowed country states to allowed country ids

--- shopinvader_locomotive_country_state/components/shopinvader_partner_export_mapper.py
+++ shopinvader_locomotive_country_state/components/shopinvader_partner_export_mapper.py
@@ -10,12 +10,27 @@ class ShopinvaderSiteExportMapper(Component):
     _usage = "export.mapper"
 
     @mapping
-    @changed_by("allowed_country_state_ids")
-    def state(self, record):
-        return {
-            "available_country_states": self._m2m_to_external(
-                record,
-                "allowed_country_state_ids",
-                ["id", "name", ("country_id", ["id"])],
-            )
-        }
+    @changed_by("allowed_country_ids", "allowed_country_state_ids")
+    def country(self, record):
+        # Overwrite method to nest available states on each available country
+        res = {}
+        for lang in record.lang_ids:
+            res[lang.code[0:2]] = []
+            for country in record.with_context(
+                lang=lang.code
+            ).allowed_country_ids:
+                states = []
+                for state in record.allowed_country_state_ids.filtered(
+                    lambda x: x.country_id == country
+                ):
+                    states.append(
+                        {
+                            "name": state.name,
+                            "id": state.id,
+                            "code": state.code,
+                        }
+                    )
+                res[lang.code[0:2]].append(
+                    {"name": country.name, "id": country.id, "states": states}
+                )
+        return {"available_countries": res}
--- shopinvader_locomotive_country_state/tests/test_backend.py
+++ shopinvader_locomotive_country_state/tests/test_backend.py
@@ -29,12 +29,7 @@ class TestBackend(LocoCommonCase):
         ref = cls.env.ref
         cls.odoo_url = cls.env["ir.config_parameter"].get_param("web.base.url")
         cls.api_url = "{}/shopinvader".format(cls.odoo_url)
-        country_ids = [ref("base.us").id]
-        state_ids = (
-            cls.env["res.country.state"]
-            .search([("country_id", "in", country_ids)])
-            .ids
-        )
+        country_us_id = ref("base.us")
         cls.backend.write(
             {
                 # Fix test demo data compat:
@@ -45,8 +40,10 @@ class TestBackend(LocoCommonCase):
                 # that tie a specific backend to the main demo backend.
                 "se_backend_id": False,
                 # ---------------------
-                "allowed_country_ids": [(6, 0, country_ids)],
-                "allowed_country_state_ids": [(6, 0, state_ids)],
+                "allowed_country_ids": [(6, 0, country_us_id.ids)],
+                "allowed_country_state_ids": [
+                    (6, 0, country_us_id.state_ids[:-1].ids)
+                ],
             }
         )
 
@@ -78,334 +75,39 @@ class TestBackend(LocoCommonCase):
         return metafields
 
     def test_synchronize_country_state_metadata(self):
-        ref = self.env.ref
         with mock_site_api(self.base_url, self.site) as mock:
             self.backend.synchronize_metadata()
             metafields = self._extract_metafields(
                 mock.request_history[0].json()["site"]["metafields"]
             )
-            expected_available_country_states = {
+            expected_available_countries = {
                 "en": [
                     {
-                        "id": ref("base.state_us_1").id,
-                        "name": "Alabama",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_2").id,
-                        "name": "Alaska",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_3").id,
-                        "name": "Arizona",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_4").id,
-                        "name": "Arkansas",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_5").id,
-                        "name": "California",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_6").id,
-                        "name": "Colorado",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_7").id,
-                        "name": "Connecticut",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_8").id,
-                        "name": "Delaware",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_9").id,
-                        "name": "District of Columbia",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_10").id,
-                        "name": "Florida",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_11").id,
-                        "name": "Georgia",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_12").id,
-                        "name": "Hawaii",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_13").id,
-                        "name": "Idaho",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_14").id,
-                        "name": "Illinois",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_15").id,
-                        "name": "Indiana",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_16").id,
-                        "name": "Iowa",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_17").id,
-                        "name": "Kansas",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_18").id,
-                        "name": "Kentucky",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_19").id,
-                        "name": "Louisiana",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_20").id,
-                        "name": "Maine",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_21").id,
-                        "name": "Montana",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_22").id,
-                        "name": "Nebraska",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_23").id,
-                        "name": "Nevada",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_24").id,
-                        "name": "New Hampshire",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_25").id,
-                        "name": "New Jersey",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_26").id,
-                        "name": "New Mexico",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_27").id,
-                        "name": "New York",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_28").id,
-                        "name": "North Carolina",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_29").id,
-                        "name": "North Dakota",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_30").id,
-                        "name": "Ohio",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_31").id,
-                        "name": "Oklahoma",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_32").id,
-                        "name": "Oregon",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_33").id,
-                        "name": "Maryland",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_34").id,
-                        "name": "Massachusetts",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_35").id,
-                        "name": "Michigan",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_36").id,
-                        "name": "Minnesota",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_37").id,
-                        "name": "Mississippi",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_38").id,
-                        "name": "Missouri",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_39").id,
-                        "name": "Pennsylvania",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_40").id,
-                        "name": "Rhode Island",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_41").id,
-                        "name": "South Carolina",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_42").id,
-                        "name": "South Dakota",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_43").id,
-                        "name": "Tennessee",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_44").id,
-                        "name": "Texas",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_45").id,
-                        "name": "Utah",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_46").id,
-                        "name": "Vermont",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_47").id,
-                        "name": "Virginia",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_48").id,
-                        "name": "Washington",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_49").id,
-                        "name": "West Virginia",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_50").id,
-                        "name": "Wisconsin",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_51").id,
-                        "name": "Wyoming",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_as").id,
-                        "name": "American Samoa",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_fm").id,
-                        "name": "Federated States of Micronesia",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_gu").id,
-                        "name": "Guam",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_mh").id,
-                        "name": "Marshall Islands",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_mp").id,
-                        "name": "Northern Mariana Islands",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_pw").id,
-                        "name": "Palau",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_pr").id,
-                        "name": "Puerto Rico",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_vi").id,
-                        "name": "Virgin Islands",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_aa").id,
-                        "name": "Armed Forces Americas",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_ae").id,
-                        "name": "Armed Forces Europe",
-                        "country_id": {"id": ref("base.us").id},
-                    },
-                    {
-                        "id": ref("base.state_us_ap").id,
-                        "name": "Armed Forces Pacific",
-                        "country_id": {"id": ref("base.us").id},
-                    },
+                        "name": country.name,
+                        "id": country.id,
+                        "states": [
+                            {
+                                "id": state.id,
+                                "name": state.name,
+                                "code": state.code,
+                            }
+                            for state in self.backend.allowed_country_state_ids.filtered(
+                                lambda x: x.country_id == country
+                            )
+                        ],
+                    }
+                    for country in self.backend.allowed_country_ids
                 ]
             }
             sorted_metafields_allowed_country_states = sorted(
-                json.loads(metafields["_store"]["available_country_states"])[
-                    "en"
-                ],
+                json.loads(metafields["_store"]["available_countries"])["en"][
+                    0
+                ]["states"],
                 key=lambda x: x["id"],
             )
             sorted_expected_available_country_states = sorted(
-                expected_available_country_states["en"], key=lambda x: x["id"]
+                expected_available_countries["en"][0]["states"],
+                key=lambda x: x["id"],
             )
             self.assertEquals(
                 sorted_metafields_allowed_country_states,
