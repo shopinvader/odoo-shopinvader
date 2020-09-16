8653f20e430be0ca519a423d43ec1528101615f1
Simone Orsi <simahawk@gmail.com>
2020-09-16 15:42:16 +0200

    Add shopinvader_product_variant_multi_link

---
+++ shopinvader_product_variant_multi_link/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020 (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Shopinvader Product Variant Link",
+    "summary": "Integrate product_variant_multi_link with Shopinvader",
+    "version": "13.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://shopinvader.com",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "shopinvader_product_template_multi_link",
+        "product_variant_multi_link",
+    ],
+}
---
+++ shopinvader_product_variant_multi_link/models/shopinvader_variant.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class ShopinvaderVariant(models.Model):
+    _name = "shopinvader.variant"
+    _inherit = ["shopinvader.variant", "shopinvader.product.link.mixin"]
+
+    def _get_product_links(self):
+        return self.product_variant_link_ids
+
+    def _product_link_target(self, link):
+        if link.left_product_id != self.record_id:
+            return link.left_product_id
+        else:
+            return link.right_product_id
+
+    def _product_link_target_variant(self, target):
+        return target._get_invader_variant(self.backend_id, self.lang_id.code)
