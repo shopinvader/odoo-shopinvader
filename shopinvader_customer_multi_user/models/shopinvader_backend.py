4fa90bb4acd3536df283aa559db36b8df9aa5e4d
Simone Orsi <simahawk@gmail.com>
2020-10-06 17:51:35 +0200

    s_customer_multi_user: add profile policy, improve tests
    Thanks to a new field 'multi_user_profile_policy' on the backend
    you can control the behavior of the profile partner.

--- shopinvader_customer_multi_user/models/shopinvader_backend.py
+++ shopinvader_customer_multi_user/models/shopinvader_backend.py
@@ -20,3 +20,26 @@ class ShopinvaderBackend(models.Model):
         "and use it for invoicing or shipping. "
         "Addresses must be validated before frontend users can use them.",
     )
+    multi_user_profile_policy = fields.Selection(
+        selection=[
+            ("main_partner", "Main partner"),
+            ("user_partner", "User's partner"),
+        ],
+        default="main_partner",
+        help="This affects the behavior of the /customer endpoint "
+        "as well as the behavior of any service acting on the current partner.\n"
+        "\n`Main partner`: the main profile for the shop frontend "
+        "will be the main partner (usually the company). "
+        "\nThis means:\n"
+        "* orders will be assigned to the main account\n"
+        "* in the profile form you'll see the main account data "
+        "but you'll be able to edit only if permissions allow to.\n"
+        "\n`User's partner`: the main profile for the shop frontend "
+        "will be the partner of the current user. "
+        "\nThis means:\n"
+        "* orders will be assigned to the specific partner\n"
+        "* in the profile form you'll see the your own account data "
+        "and you'll be able to edit\n"
+        "* profile info will include a reference to the main account "
+        "inside the `main_account` key.",
+    )
--- shopinvader_customer_multi_user/tests/test_multi_user_customer.py
+++ shopinvader_customer_multi_user/tests/test_multi_user_customer.py
@@ -2,55 +2,12 @@
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-
-from odoo.addons.shopinvader.tests.test_customer import TestCustomerCommon
-
-_logger = logging.getLogger(__name__)
+from .common import TestMultiUserCommon
 
 
-class TestCustomer(TestCustomerCommon):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.company_binding = cls._create_partner(
-            cls.env, invader_user_token="ABCDEF"
-        )
-        cls.company = cls.company_binding.record_id
-
-    @staticmethod
-    def _create_partner(env, **kw):
-        values = {
-            "backend_id": env.ref("shopinvader.backend_1").id,
-            "is_company": True,
-            "name": "ACME ltd",
-            "external_id": "acme",
-            "email": "company@test.com",
-            "ref": "#ACME",
-        }
-        values.update(kw)
-        return env["shopinvader.partner"].create(values)
-
-    def test_token_manual_override(self):
-        self.assertEqual(self.company.invader_user_token, "ABCDEF")
-
-    def _generate_random_companies(self, count=5):
-        tokens = set()
-        for x in range(5):
-            comp = self._create_partner(self.env, email="acme%s@test.com" % x)
-            if comp.invader_user_token:
-                tokens.add(comp.invader_user_token)
-        return tokens
-
-    def test_token_auto_gen_disabled(self):
-        self.assertFalse(self.backend.customer_multi_user)
-        tokens = self._generate_random_companies()
-        self.assertEqual(len(tokens), 0)
-
-    def test_token_auto_gen(self):
-        self.backend.customer_multi_user = True
-        tokens = self._generate_random_companies()
-        self.assertEqual(len(tokens), 5)
+class TestMultiUserCustomer(TestMultiUserCommon):
+    """Test interaction with /customer endpoint.
+    """
 
     def test_create_customer_no_multi_user(self):
         self.data.update({"external_id": "cust1"})
@@ -103,28 +60,27 @@ class TestCustomer(TestCustomerCommon):
         self.assertNotIn(partner, self.company.child_ids)
         self.assertFalse(self.company.is_invader_user)
 
-    def test_get_customer_partner(self):
-        self.backend.customer_multi_user = True
-        data = dict(
-            self.data, external_id="cust1", email="new@one.com", name="New One"
-        )
-        # customer 1
-        params = dict(data, company_token="ABCDEF")
-        res = self.service.dispatch("create", params=params)["data"]
-        partner1 = self.env["res.partner"].browse(res["id"])
-        binding1 = partner1._get_invader_partner(self.backend)
-        # partner is the parent company
-        self.assertTrue(binding1.is_invader_user)
-        self.assertEqual(
-            partner1.get_customer_partner(self.backend), self.company
-        )
-        self.assertEqual(
-            self.company.get_customer_partner(self.backend), self.company
-        )
-
     def test_customer_data(self):
         res = self.service._to_customer_info(self.company)
+        # multi user not enabled
+        self.assertNotIn("company_token", res)
+        self.assertNotIn("main_account", res)
+        self.assertNotIn("is_simple_user", res)
+
+        # enable it
+        self.backend.customer_multi_user = True
+
+        # check on the company
+        res = self.service._to_customer_info(self.company)
         self.assertEqual(res["company_token"], "ABCDEF")
+        self.assertFalse(res["is_simple_user"])
+        # same user of the company
+        self.assertEqual(res["main_account"], None)
+
+        # check on a simple user
+        res = self.service._to_customer_info(self.invader_user.record_id)
+        self.assertTrue(res["is_simple_user"])
+        self.assertNotIn("company_token", res)
         self.assertEqual(
             res["main_account"],
             {
