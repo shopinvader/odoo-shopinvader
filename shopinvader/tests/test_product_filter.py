0a91670b30c7500b06475ec78c0ac4c0efe8e30f
Simone Orsi <simahawk@gmail.com>
2020-10-20 15:47:50 +0200

    shopinvader: product.filter improve tests

---
+++ shopinvader/tests/test_product_filter.py
@@ -0,0 +1,47 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from .common import CommonCase
+
+
+class TestProductFilter(CommonCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.shopinvader_product_field = cls.env["ir.model.fields"].search(
+            [("model", "=", "shopinvader.product"), ("name", "=", "name")],
+            limit=1,
+        )
+        cls.variant_attribute = cls.env["product.attribute"].search(
+            [("name", "=", "Legs")], limit=1
+        )
+        cls._install_lang(cls, "base.lang_fr")
+
+        cls.variant_attribute.with_context(lang="fr_FR").name = "Jambes"
+
+        cls.filter_on_field = cls.env["product.filter"].create(
+            {
+                "name": "Product Name",
+                "based_on": "field",
+                "field_id": cls.shopinvader_product_field.id,
+            }
+        )
+        cls.filter_on_attr = cls.env["product.filter"].create(
+            {
+                "name": "Test Filter on field name",
+                "based_on": "variant_attribute",
+                "variant_attribute_id": cls.variant_attribute.id,
+            }
+        )
+
+    def test_product_filter_field_name(self):
+        self.assertEqual(self.filter_on_field.display_name, "name")
+
+    def test_product_filter_attribute_name(self):
+        self.assertEqual(
+            self.filter_on_attr.display_name, "variant_attributes.legs"
+        )
+        self.assertEqual(
+            self.filter_on_attr.with_context(lang="fr_FR").display_name,
+            "variant_attributes.jambes",
+        )
