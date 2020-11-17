cd2fb88aff84be4f2d14e51a49bc31a7843dcf84
Simone Orsi <simahawk@gmail.com>
2020-11-17 14:00:28 +0100

    s_algolia: fix _get_facetting_values call

--- shopinvader_algolia/models/se_index.py
+++ shopinvader_algolia/models/se_index.py
@@ -12,8 +12,11 @@ class SeIndex(models.Model):
         data = super(SeIndex, self)._get_settings()
         model = self.model_id.model
         # TODO check backend se type and call specific algolia method?
-        facetting_values = self.env[model]._get_facetting_values(
-            self.backend_id, self.lang_id
-        )
-        data.update({"attributesForFaceting": facetting_values})
+        facetting_values = {}
+        if hasattr(self.env[model], "_get_facetting_values"):
+            facetting_values = self.env[model]._get_facetting_values(
+                self.backend_id, self.lang_id
+            )
+        if facetting_values:
+            data.update({"attributesForFaceting": facetting_values})
         return data
