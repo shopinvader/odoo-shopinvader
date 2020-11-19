2cee1fe832f660d782133c695440579f2c727569
Simone Orsi <simahawk@gmail.com>
2020-11-19 08:49:09 +0100

    s_customer_price_wishlist: support future parser
    The new parser will come w/ #783. This harmeless change
    makes the 2 versions compatible.

--- shopinvader_customer_price_wishlist/services/wishlist.py
+++ shopinvader_customer_price_wishlist/services/wishlist.py
@@ -27,3 +27,15 @@ class WishlistService(Component):
 
     def _json_parser_product_price(self, rec, fname):
         return self.customer_price_service._get_price(rec, fname)
+
+    # FIXME: this method does not exist in 13.0. It will be introduced by
+    # https://github.com/shopinvader/odoo-shopinvader/pull/783
+    # If you want to use all recent features this is required to not break it.
+    # It's harmless as it's called only when used w/ #783.
+    # When #783 will be merged, `_json_parser_product` must be dropped.
+    def _json_parser_product_data(self, rec, fname):
+        res = super()._json_parser_product_data(rec, fname)
+        res["price"] = self._json_parser_product_price(
+            rec.shopinvader_variant_id, fname
+        )
+        return res
