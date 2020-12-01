4b029d17cb086d0dafa93dd7ba338763bb91585c
Simone Orsi <simahawk@gmail.com>
2020-12-01 17:39:59 +0100

    s_sale_profile: fix and _get_cart_pricelist override

--- shopinvader_sale_profile/models/shopinvader_backend.py
+++ shopinvader_sale_profile/models/shopinvader_backend.py
@@ -1,6 +1,8 @@
 # Copyright 2018 Akretion (http://www.akretion.com).
 # Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
+# Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, exceptions, fields, models
 
@@ -32,6 +34,8 @@ class ShopinvaderBackend(models.Model):
 
     @api.constrains("use_sale_profile", "pricelist_id")
     def _check_use_sale_profile(self):
+        # TODO: does this make sense?
+        # We could set the pricelist in the onchange above
         if self.pricelist_id and self.use_sale_profile:
             raise exceptions.ValidationError(
                 _(
@@ -40,14 +44,31 @@ class ShopinvaderBackend(models.Model):
                 )
             )
 
-    def _get_customer_default_pricelist(self):
+    def _get_cart_pricelist(self, partner=None):
         """Override to use default profile pricelist when needed.
         """
-        pricelist = super()._get_customer_default_pricelist()
-        if self.use_sale_profile:
-            # TODO: shall we override `_selection_default_role`
-            # to list all profiles' code and pick the profile usin its code?
-            profile = self.sale_profile_ids.filtered("default")
-            if profile and profile.pricelist_id:
-                pricelist = profile.pricelist_id
+        pricelist = super()._get_cart_pricelist(partner)
+        if not self.use_sale_profile:
+            return pricelist
+        default_profile_pricelist = self._get_default_profile_pricelist()
+        # TODO: we should receive shopinvader.partner all around
+        invader_partner = (
+            partner._get_invader_partner(self)
+            if partner
+            else self.env["shopinvader.partner"].browse()
+        )
+        if invader_partner and invader_partner.sale_profile_id:
+            # If the partner has been created via shopinvader this will match
+            # property_product_pricelist already
+            return invader_partner.sale_profile_id.pricelist_id
+        elif (invader_partner and not pricelist) or not invader_partner:
+            return default_profile_pricelist or pricelist
         return pricelist
+
+    def _get_default_profile_pricelist(self):
+        # TODO: shall we override `_selection_default_role`
+        # to list all profiles' code and pick the profile usin its code?
+        profile = self.sale_profile_ids.filtered("default")
+        if profile and profile.pricelist_id:
+            return profile.pricelist_id
+        return None
