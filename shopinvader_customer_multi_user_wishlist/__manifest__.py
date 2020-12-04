659ccd47c3c599819070ebe5f83556b4d7299846
Simone Orsi <simahawk@gmail.com>
2020-12-04 15:05:30 +0100

    Add shopinvader_customer_multi_user_wishlist

---
+++ shopinvader_customer_multi_user_wishlist/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Customer Multi User Wishlist integration",
+    "summary": """
+        Integrate customer multi user and wishlist.
+    """,
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["shopinvader_customer_multi_user", "shopinvader_wishlist"],
+    "auto_install": True,
+}
---
+++ shopinvader_customer_multi_user_wishlist/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Integrate multi user behavior with wishlists.
+
+Mostly allows to add wishlists to sale order even when the partner does not match.
