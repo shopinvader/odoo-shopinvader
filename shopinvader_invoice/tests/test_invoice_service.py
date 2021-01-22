46648fb5eee60f72cc482addd487d710fd5e8f4d
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-01-22 16:57:21 +0100

    [FIX] base_rest new api compatibility

--- shopinvader_invoice/tests/test_invoice_service.py
+++ shopinvader_invoice/tests/test_invoice_service.py
@@ -175,7 +175,7 @@ class TestInvoiceService(CommonCase):
         self.assertEquals(invoice2.partner_id, self.service.partner)
         self.assertEquals(invoice3.partner_id, self.service.partner)
         self.assertEquals(invoice4.partner_id, self.service.partner)
-        result = self.service.dispatch("get", _id=invoice1.id)
+        result = self.service.dispatch("get", invoice1.id)
         data = result.get("data", [])
         self._check_data_content([data], invoice1)
         return
@@ -190,12 +190,12 @@ class TestInvoiceService(CommonCase):
         invoice1 = self._confirm_and_invoice_sale(self.sale)
         self.assertEquals(invoice1.partner_id, self.service.partner)
         # The owner can do a 'get' on it
-        self.service.dispatch("get", _id=invoice1.id)
+        self.service.dispatch("get", invoice1.id)
         # Now use another user/partner
         with self.work_on_services(partner=self.partner2) as work:
             self.service = work.component(usage="invoice")
         with self.assertRaises(exceptions.MissingError) as cm:
-            self.service.dispatch("get", _id=invoice1.id)
+            self.service.dispatch("get", invoice1.id)
         self.assertIn("does not exist", cm.exception.name)
         self.assertIn(str(invoice1.id), cm.exception.name)
         return
