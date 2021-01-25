c3327e0383aa813edd65f9cbde2b5f292cd30159
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-25 11:38:54 +0100

    address default type as contact

--- shopinvader/services/address.py
+++ shopinvader/services/address.py
@@ -85,7 +85,7 @@ class AddressService(Component):
             "type": {
                 "type": "string",
                 "allowed": self._get_allowed_type(),
-                "default": "other",
+                "default": "contact",
             },
             "street": {"type": "string", "required": True, "empty": False},
             "street2": {"type": "string", "nullable": True},
