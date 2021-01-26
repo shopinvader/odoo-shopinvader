5216f49c6b6a51762825a8c6d616e6c055225c11
Chafique <chafique.delli@akretion.com>
2021-01-26 15:48:53 +0100

    [IMP] shopinvader_sale_profile: black, isort, prettier

--- shopinvader_sale_profile/models/shopinvader_partner.py
+++ shopinvader_sale_profile/models/shopinvader_partner.py
@@ -74,9 +74,7 @@ class ShopinvaderPartner(models.Model):
             shopinv_partners = self.filtered(
                 lambda p, c=company: p.backend_id.company_id.id == c.id
             )
-            shopinv_partners = shopinv_partners.with_context(
-                force_company=company.id
-            )
+            shopinv_partners = shopinv_partners.with_context(force_company=company.id)
             for binding in shopinv_partners:
                 sale_profile = sale_profile_obj.browse()
                 # Only if the related backend use sale profiles
@@ -114,9 +112,7 @@ class ShopinvaderPartner(models.Model):
                 and p.pricelist_id.id == pl.id
                 and p.backend_id.id == b.id
             )
-            sale_profile = first(
-                sale_profile.with_prefetch(self._prefetch_ids)
-            )
+            sale_profile = first(sale_profile.with_prefetch(self._prefetch_ids))
         else:
             pricelist = partner.property_product_pricelist
             sale_profile = sale_profiles.filtered(
@@ -124,17 +120,13 @@ class ShopinvaderPartner(models.Model):
                 and p.pricelist_id.id == pl.id
                 and p.backend_id.id == b.id
             )
-            sale_profile = first(
-                sale_profile.with_prefetch(self._prefetch_ids)
-            )
+            sale_profile = first(sale_profile.with_prefetch(self._prefetch_ids))
         if not sale_profile:
             # Get the default sale profile
             sale_profile = default_sale_profiles.filtered(
                 lambda p, b=backend: p.backend_id.id == b.id
             )
-            sale_profile = first(
-                sale_profile.with_prefetch(self._prefetch_ids)
-            )
+            sale_profile = first(sale_profile.with_prefetch(self._prefetch_ids))
             if not sale_profile:
                 message = (
                     _("No default sale profile found for the backend" " %s")
@@ -143,9 +135,7 @@ class ShopinvaderPartner(models.Model):
                 raise exceptions.UserError(message)
         return sale_profile
 
-    def _get_sale_profiles(
-        self, backend_ids, pricelists, fposition_ids, company=False
-    ):
+    def _get_sale_profiles(self, backend_ids, pricelists, fposition_ids, company=False):
         """Get sale profiles for given backends, fiscal positions, pricelists.
 
         :param fposition_ids: list of int
@@ -156,9 +146,7 @@ class ShopinvaderPartner(models.Model):
         """
         sale_profile_obj = self.env["shopinvader.sale.profile"]
         if company:
-            sale_profile_obj = sale_profile_obj.with_context(
-                force_company=company.id
-            )
+            sale_profile_obj = sale_profile_obj.with_context(force_company=company.id)
         domain = [
             "|",
             ("fiscal_position_ids", "in", fposition_ids),
@@ -181,9 +169,7 @@ class ShopinvaderPartner(models.Model):
             ("default", "=", True),
             ("backend_id", "in", backend_ids),
         ]
-        default_sale_profiles = sale_profile_obj.search(
-            domain_default_profiles
-        )
+        default_sale_profiles = sale_profile_obj.search(domain_default_profiles)
         return default_sale_profiles
 
     @api.model
@@ -196,9 +182,7 @@ class ShopinvaderPartner(models.Model):
         """
         fposition_obj = self.env["account.fiscal.position"]
         if company_id:
-            fposition_obj = fposition_obj.with_context(
-                force_company=company_id
-            )
+            fposition_obj = fposition_obj.with_context(force_company=company_id)
         fposition_by_partner = {}
         for partner in partners:
             fpos_id = fposition_obj.get_fiscal_position(
--- shopinvader_sale_profile/tests/test_shopinvader_backend_sale_profile.py
+++ shopinvader_sale_profile/tests/test_shopinvader_backend_sale_profile.py
@@ -2,6 +2,7 @@
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import mock
+
 from odoo.addons.shopinvader.tests.common import CommonCase
 
 
@@ -17,12 +18,8 @@ class TestShopinvaderBackendSaleProfile(CommonCase):
 
     def test_compute_pricelist(self):
         self.backend.write({"use_sale_profile": True})
-        profile1 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_1"
-        )
-        profile2 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_2"
-        )
+        profile1 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_1")
+        profile2 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_2")
         self.assertNotEqual(profile1.pricelist_id, profile2.pricelist_id)
         self.assertTrue(profile1.default)
         expected = profile1.pricelist_id
@@ -34,12 +31,8 @@ class TestShopinvaderBackendSaleProfile(CommonCase):
 
     def test_get_pricelist(self):
         self.backend.write({"use_sale_profile": True})
-        profile1 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_1"
-        )
-        profile2 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_2"
-        )
+        profile1 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_1")
+        profile2 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_2")
         expected = profile1.pricelist_id
         self.assertEqual(self.backend._get_cart_pricelist(), expected)
         profile1.default = False
@@ -54,12 +47,8 @@ class TestShopinvaderBackendSaleProfile(CommonCase):
 
     def test_get_pricelist_partner(self):
         self.backend.write({"use_sale_profile": True})
-        profile1 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_1"
-        )
-        profile2 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_2"
-        )
+        profile1 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_1")
+        profile2 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_2")
         partner = self.env.ref("shopinvader.partner_2")
         invader_partner = partner._get_invader_partner(self.backend)
         # Ingnore how sale profile is computed, just simulate its value
@@ -70,23 +59,15 @@ class TestShopinvaderBackendSaleProfile(CommonCase):
         ) as mocked:
             mocked.return_value = profile1
             expected = profile1.pricelist_id
-            self.assertEqual(
-                self.backend._get_cart_pricelist(partner), expected
-            )
+            self.assertEqual(self.backend._get_cart_pricelist(partner), expected)
             mocked.return_value = profile2
             expected = profile2.pricelist_id
-            self.assertEqual(
-                self.backend._get_cart_pricelist(partner), expected
-            )
+            self.assertEqual(self.backend._get_cart_pricelist(partner), expected)
 
     def test_get_pricelist_partner_no_profile(self):
         self.backend.write({"use_sale_profile": True})
-        profile1 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_1"
-        )
-        profile2 = self.env.ref(
-            "shopinvader_sale_profile.shopinvader_sale_profile_2"
-        )
+        profile1 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_1")
+        profile2 = self.env.ref("shopinvader_sale_profile.shopinvader_sale_profile_2")
         partner = self.env.ref("shopinvader.partner_2")
         invader_partner = partner._get_invader_partner(self.backend)
         # Ingnore how sale profile is computed, just simulate its value
@@ -97,12 +78,11 @@ class TestShopinvaderBackendSaleProfile(CommonCase):
         ) as mocked:
             mocked.return_value = None
             expected = profile1.pricelist_id
-            self.assertEqual(
-                self.backend._get_cart_pricelist(partner), expected
-            )
+            self.assertEqual(self.backend._get_cart_pricelist(partner), expected)
             profile1.default = False
             profile2.default = True
             expected = profile2.pricelist_id
             self.assertEqual(
-                self.backend._get_cart_pricelist(partner), expected,
+                self.backend._get_cart_pricelist(partner),
+                expected,
             )
