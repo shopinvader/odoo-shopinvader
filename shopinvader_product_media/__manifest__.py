b4d4af21a160de1bd7c47eba8568795986dbc835
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-28 09:48:55 +0100

    [MIG] shopinvader_product_media: Migration to 14.0

--- shopinvader_product_media/__manifest__.py
+++ shopinvader_product_media/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product Media",
     "summary": "Index storage media data into external search engine",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion",
