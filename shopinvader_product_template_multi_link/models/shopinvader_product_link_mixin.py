5e95934bed2d70ef3f4e022199e1744cf4ecf849
Chafique <chafique.delli@akretion.com>
2021-02-02 11:43:26 +0100

    [IMP] shopinvader_product_template_multi_link: black, isort, prettier

--- shopinvader_product_template_multi_link/models/shopinvader_product_link_mixin.py
+++ shopinvader_product_template_multi_link/models/shopinvader_product_link_mixin.py
@@ -9,6 +9,7 @@
 from collections import defaultdict
 
 from odoo import models
+
 from odoo.addons.base_sparse_field.models.fields import Serialized
 from odoo.addons.http_routing.models.ir_http import slugify
 
