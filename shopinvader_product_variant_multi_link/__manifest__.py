7fd4c6163cfa21b1815f16c38e95e9a9dd7babb0
Chafique <chafique.delli@akretion.com>
2021-02-04 10:39:10 +0100

    [MIG] shopinvader_product_variant_multi_link: Migration to 14.0

--- shopinvader_product_variant_multi_link/__manifest__.py
+++ shopinvader_product_variant_multi_link/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Shopinvader Product Variant Link",
     "summary": "Integrate product_variant_multi_link with Shopinvader",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Camptocamp",
