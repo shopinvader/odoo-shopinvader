59ae5c46eff7d76b3154767f50667fc1259356f3
David Popa <davidpopa404@gmail.com>
2021-02-10 11:38:30 +0100

    [IMP] shopinvader_multi_category: black, isort, prettier

--- shopinvader_multi_category/__manifest__.py
+++ shopinvader_multi_category/__manifest__.py
@@ -11,6 +11,6 @@
     "author": "Akretion",
     "license": "AGPL-3",
     "depends": ["shopinvader", "product_multi_category"],
-    "installable": False,
+    "installable": True,
     "auto_install": True,
 }
