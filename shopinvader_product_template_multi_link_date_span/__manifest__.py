b2977f72d573f1bb6a1050b88f6a70f94309b903
Simone Orsi <simahawk@gmail.com>
2021-02-17 14:10:25 +0100

    Add shopinvader_product_template_multi_link_date_span

---
+++ shopinvader_product_template_multi_link_date_span/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Product Link Date Span",
+    "summary": "Integrate `product_template_multi_link_date_span` in Shopinvader",
+    "version": "13.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "shopinvader_product_template_multi_link",
+        "product_template_multi_link_date_span",
+    ],
+}
