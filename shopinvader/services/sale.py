ebc4491c8b644610db21930b2f7cc7bcca37749a
Pierrick Brun <pierrick.brun@akretion.com>
2021-03-03 13:54:02 +0100

    [ADD] cancel and redraft sale endpoint

--- shopinvader/services/sale.py
+++ shopinvader/services/sale.py
@@ -1,7 +1,9 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo.exceptions import UserError
 from odoo.osv import expression
+from odoo.tools.translate import _
 
 from odoo.addons.base_rest.components.service import to_int
 from odoo.addons.component.core import Component
@@ -49,6 +51,21 @@ class SaleService(Component):
         self._ask_email_invoice = True
         return self.ask_email(_id)
 
+    def cancel(self, _id):
+        order = self._get(_id)
+        self._cancel(order)
+        return self._to_json(order)[0]
+
+    def reset_to_cart(self, _id):
+        order = self._get(_id)
+        self._cancel(order, reset_to_cart=True)
+        res = self._to_json(order)[0]
+        return {
+            "data": res,
+            "set_session": {"cart_id": res["id"]},
+            "store_cache": {"cart": res},
+        }
+
     # Validator
     def _validator_search(self):
         default_search_validator = self._default_validator_search()
@@ -58,6 +75,12 @@ class SaleService(Component):
 
     def _validator_ask_email_invoice(self):
         return self._validator_ask_email()
+
+    def _validator_cancel(self):
+        return {}
+
+    def _validator_reset_to_cart(self):
+        return {}
 
     # The following method are 'private' and should be never never NEVER call
     # from the controller.
@@ -101,6 +124,18 @@ class SaleService(Component):
         invoice_service = self.component(usage="invoice")
         domain_state = invoice_service._get_domain_state()
         return invoices.filtered_domain(domain_state)
+
+    def _cancel(self, sale, reset_to_cart=False):
+        for line in sale.order_line:
+            if line.qty_delivered > 0 or line.qty_invoiced > 0:
+                raise UserError(
+                    _("Orders that have been delivered or invoiced cannot be edited.")
+                )
+        sale.action_cancel()
+        if reset_to_cart:
+            sale.action_draft()
+            sale.typology = "cart"
+        return sale
 
     def _convert_invoices(self, invoices):
         return [self._convert_one_invoice(invoice) for invoice in invoices]
