9be7f2dfa5f55737ee3f3fe24ec022dddeaeb873
Pierrick Brun <pierrick.brun@akretion.com>
2021-03-15 12:10:45 +0100

    [MIG] shopinvader_elasticsearch: Migration to 14.0

--- shopinvader_elasticsearch/__manifest__.py
+++ shopinvader_elasticsearch/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Shopinvader Elasticsearch",
     "summary": """
         Shopinvader Elasticsearch Connector""",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
