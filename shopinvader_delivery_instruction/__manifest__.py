3687015f77eb0db481de572e0ad86d8fd16e600a
Denis Roussel <denis.roussel@acsone.eu>
2021-03-26 11:22:21 +0100

    [12.0][FIX] shopinvader_delivery_instruction: Pre-commit

--- shopinvader_delivery_instruction/__manifest__.py
+++ shopinvader_delivery_instruction/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Shopinvader delivery instruction",
-    "description": """Shopinvader addons to let user define delivery
+    "summary": """Shopinvader addons to let user define delivery
     instructions""",
     "author": "ACSONE SA/NV",
-    "website": "http://shopinvader.com/",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
     "category": "e-commerce",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
