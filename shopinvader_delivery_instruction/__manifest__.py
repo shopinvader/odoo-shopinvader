e349517126a81751710149bf66b5533a3d0903c9
Sébastien BEAU <sebastien.beau@akretion.com>
2021-03-26 12:15:04 +0100

    [MIG] shopinvader_delivery_instruction: Migration to 13.0

--- shopinvader_delivery_instruction/__manifest__.py
+++ shopinvader_delivery_instruction/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "category": "e-commerce",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "shopinvader",
