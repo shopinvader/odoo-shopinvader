2ffe0059ef81b1410fbd7b46fe47b2d1044c015b
Simone Orsi <simahawk@gmail.com>
2021-04-01 16:17:57 +0200

    s_customer_multi_user: only owners and admin can edit addresses

--- shopinvader_customer_multi_user/components/access_info.py
+++ shopinvader_customer_multi_user/components/access_info.py
@@ -12,3 +12,19 @@ class PartnerAccess(Component):
         perm = super().permissions()
         perm["users"] = {"manage": self.invader_partner_user.is_users_manager}
         return perm
+
+    def for_address(self, address_id):
+        info = super().for_address(address_id)
+        # Who can edit
+        # - owner
+        # - admin account
+        if self.partner_user is not None:
+            can_manage = (
+                self.is_owner(address_id)
+                or self.invader_partner_user.is_admin_account
+            )
+            if can_manage:
+                info.update({"read": True, "update": True, "delete": True})
+            else:
+                info.update({"read": True, "update": False, "delete": False})
+        return info
--- shopinvader_customer_multi_user/tests/test_partner_access_info.py
+++ shopinvader_customer_multi_user/tests/test_partner_access_info.py
@@ -38,3 +38,53 @@ class TestPartnerAccessInfo(TestUserManagementCommmon):
             partner_user=binding.record_id, invader_partner_user=binding,
         ).permissions()
         self.assertTrue(info["users"]["manage"])
+
+    def test_access_info_address_admin(self):
+        """Admin can always CRUD addresses."""
+        for binding in (
+            self.user_binding + self.user_binding2 + self.user_binding3
+        ):
+            info = self._get_access_info().for_address(binding.record_id)
+            expected = {"read": True, "update": True, "delete": True}
+            self.assertEqual(info, expected)
+
+    def test_access_info_address_simpleuser_owner(self):
+        """Owners can always CRUD their addresses."""
+        for binding in (
+            self.user_binding + self.user_binding2 + self.user_binding3
+        ):
+            info = self._get_access_info(
+                partner_user=binding.record_id, invader_partner_user=binding,
+            ).for_address(binding.record_id.id)
+            # They can edit their own record
+            expected = {"read": True, "update": True, "delete": True}
+            self.assertEqual(info, expected)
+
+    def test_access_info_address_simpleuser_not_owner(self):
+        """Owners can only READ others' addresses."""
+        binding = self.user_binding
+        access_info = self._get_access_info(
+            partner_user=binding.record_id, invader_partner_user=binding,
+        )
+        for binding in self.user_binding2 + self.user_binding3:
+            info = access_info.for_address(binding.record_id.id)
+            expected = {"read": True, "update": False, "delete": False}
+            self.assertEqual(info, expected)
+
+        binding = self.user_binding2
+        access_info = self._get_access_info(
+            partner_user=binding.record_id, invader_partner_user=binding,
+        )
+        for binding in self.user_binding + self.user_binding3:
+            info = access_info.for_address(binding.record_id.id)
+            expected = {"read": True, "update": False, "delete": False}
+            self.assertEqual(info, expected)
+
+        binding = self.user_binding3
+        access_info = self._get_access_info(
+            partner_user=binding.record_id, invader_partner_user=binding,
+        )
+        for binding in self.user_binding + self.user_binding2:
+            info = access_info.for_address(binding.record_id.id)
+            expected = {"read": True, "update": False, "delete": False}
+            self.assertEqual(info, expected)
