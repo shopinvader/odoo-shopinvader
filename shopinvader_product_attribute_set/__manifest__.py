b16955004d4ba7c967ab80ad649e8444282aa460
Sébastien BEAU <sebastien.beau@akretion.com>
2021-06-17 09:03:44 +0200

    [MIG] shopinvader_product_attribute_set: Migration to 14.0

--- shopinvader_product_attribute_set/__manifest__.py
+++ shopinvader_product_attribute_set/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopinvader Product Attribute Set",
     "summary": "Expose all PIM' Attribute sets with Shopinvader",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Akretion",
