1e0467b961094db3e087ee68d76dce1a56ffa38e
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-06 16:06:24 -0300

    [ADD] shopinvader_sale_coupon

---
+++ shopinvader_sale_coupon/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Camptocamp SA
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopvinvader Sale Coupon",
+    "summary": "Manage Promotion and Coupon programs in Shopinvader",
+    "version": "14.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "depends": ["shopinvader", "sale_coupon"],
+}
