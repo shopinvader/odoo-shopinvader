6c755237bb1793d9b0502f383c91c433d26c4877
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-13 13:24:42 -0300

    [ADD] shopinvader_product_template_tags

---
+++ shopinvader_product_template_tags/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Camptocamp SA
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopvinvader Product Template Tags",
+    "summary": "Index Product Template Tags in Shopinvader",
+    "version": "14.0.1.0.0",
+    "category": "e-Commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "depends": ["shopinvader", "product_template_tags"],
+    "data": ["data/ir_export_product.xml"],
+}
---
+++ shopinvader_product_template_tags/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Index Product Template Tags in Shopinvader.
+Tags are defined in OCA module `product_template_tags`
