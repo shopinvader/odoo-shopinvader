376d3afe190fa4d30eb12b6b29423f2d8e6d8abb
Simone Orsi <simahawk@gmail.com>
2021-07-15 10:21:55 +0200

    Add shopinvader_customer_multi_user_validate

---
+++ shopinvader_customer_multi_user_validate/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Customer Multi User Validate",
+    "summary": """
+    Glue module for `shopinvader_customer_validate` and `shopinvader_customer_multi_user`.
+    """,
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["shopinvader_customer_validate", "shopinvader_customer_multi_user"],
+    "installable": True,
+    "auto_install": True,
+}
