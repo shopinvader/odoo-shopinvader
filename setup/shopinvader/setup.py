1e001cf5855a2da24c59d3349666f487d8846cae
Denis Roussel <denis.roussel@acsone.eu>
2021-08-03 16:31:39 +0200

    [14.0][IMP] shopinvader: Minimum version of base_rest

--- setup/shopinvader/setup.py
+++ setup/shopinvader/setup.py
@@ -2,5 +2,9 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'depends_override': {
+            'base_rest': 'odoo14-addon-base-rest>=14.0.4.0.0',
+        },
+    }
 )
