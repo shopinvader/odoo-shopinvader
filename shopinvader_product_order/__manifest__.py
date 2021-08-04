415adf728ac05430c8af8f9cb290f767d5cd782b
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-08-04 15:14:21 -0300

    [ADD] shopinvader_product_order

---
+++ shopinvader_product_order/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Camptocamp (http://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Product Order",
+    "summary": "Manage product display order on Shopinvader",
+    "version": "14.0.1.0.0",
+    "category": "Shopinvader",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "depends": ["shopinvader"],
+    "data": [
+        "data/ir_export_product.xml",
+        "views/shopinvader_product.xml",
+    ],
+}
