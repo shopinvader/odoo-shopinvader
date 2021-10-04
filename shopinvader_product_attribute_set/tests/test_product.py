78f5dca9629ab689042c265d7b921ffcbaee93df
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2021-10-04 17:30:04 +0200

    [IMP] Add native attribute tests

--- shopinvader_product_attribute_set/tests/test_product.py
+++ shopinvader_product_attribute_set/tests/test_product.py
@@ -24,6 +24,12 @@ class ProductCase(ProductCommonCase):
             in ["x_linux_compatible", "x_processor", "x_technical_description"]
         )
         self.attr_set.attribute_ids = attributes
+        self.template_group = self.env["attribute.group"].create(
+            {
+                "name": "Test Group",
+                "model_id": self.env.ref("product.model_product_template").id,
+            }
+        )
 
     def test_product_attributes(self):
         self.product.write(
@@ -100,4 +106,85 @@ class ProductCase(ProductCommonCase):
                 "key": "processor",
                 "type": "select",
             },
+        )
+
+    def test_native_attribute_char(self):
+        self.env["attribute.attribute"].create(
+            {
+                "nature": "native",
+                "field_id": self.env.ref(
+                    "product.field_product_template__default_code"
+                ).id,
+                "attribute_set_ids": [(6, 0, [self.attr_set.id])],
+                "attribute_group_id": self.template_group.id,
+            }
+        )
+        self.product.write({"attribute_set_id": self.attr_set.id})
+        self.assertEqual(
+            self.shopinvader_variant.attributes["default_code"],
+            self.product.default_code,
+        )
+
+    def test_native_attribute_many2one(self):
+        self.env["attribute.attribute"].create(
+            {
+                "nature": "native",
+                "field_id": self.env.ref(
+                    "product.field_product_template__categ_id"
+                ).id,
+                "attribute_set_ids": [(6, 0, [self.attr_set.id])],
+                "attribute_group_id": self.template_group.id,
+            }
+        )
+        self.product.write({"attribute_set_id": self.attr_set.id})
+        self.assertEqual(
+            self.shopinvader_variant.attributes["categ_id"],
+            self.product.categ_id.name,
+        )
+
+    def test_native_attribute_boolean(self):
+        self.env["attribute.attribute"].create(
+            {
+                "nature": "native",
+                "field_id": self.env.ref(
+                    "product.field_product_template__rental"
+                ).id,
+                "attribute_set_ids": [(6, 0, [self.attr_set.id])],
+                "attribute_group_id": self.template_group.id,
+            }
+        )
+        self.product.write({"attribute_set_id": self.attr_set.id})
+        self.assertEqual(
+            self.shopinvader_variant.attributes["rental"], self.product.rental
+        )
+        rental_field = {}
+        for field in self.shopinvader_variant.structured_attributes[1][
+            "fields"
+        ]:
+            if field["key"] == "rental":
+                rental_field = field
+        self.assertEqual(
+            rental_field,
+            {
+                "value": "false",
+                "name": "Can be Rent",
+                "key": "rental",
+                "type": "boolean",
+            },
+        )
+
+    def test_native_attribute_selection(self):
+        self.env["attribute.attribute"].create(
+            {
+                "nature": "native",
+                "field_id": self.env.ref(
+                    "product.field_product_template__type"
+                ).id,
+                "attribute_set_ids": [(6, 0, [self.attr_set.id])],
+                "attribute_group_id": self.template_group.id,
+            }
+        )
+        self.product.write({"attribute_set_id": self.attr_set.id})
+        self.assertEqual(
+            self.shopinvader_variant.attributes["type"], self.product.type,
         )
