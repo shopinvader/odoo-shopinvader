38c03dd97e2289687b772dc2876e621f232a85ed
François Honoré <francois.honore@acsone.eu>
2021-11-05 10:30:38 +0100

    [14.0][IMP] shopinvader_guest_mode: disable welcome message for guest

--- shopinvader_guest_mode/services/__init__.py
+++ shopinvader_guest_mode/services/__init__.py
@@ -1,3 +1,4 @@
 from . import customer
 from . import guest_service
 from . import address
+from . import partner_mixin
--- shopinvader_guest_mode/tests/__init__.py
+++ shopinvader_guest_mode/tests/__init__.py
@@ -1,3 +1,4 @@
 from . import test_shopinvader_backend
 from . import test_shopinvader_partner
 from . import test_guest_service
+from . import test_notification
