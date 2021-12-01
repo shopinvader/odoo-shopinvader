ed0a1183304bcc0f6e58e53ffc4eb0e34dcb05ba
Simone Orsi <simahawk@gmail.com>
2021-12-01 14:41:40 +0100

    s_product_template_multi_link_date_span: migrate to v14

--- shopinvader_product_template_multi_link_date_span/__manifest__.py
+++ shopinvader_product_template_multi_link_date_span/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product Link Date Span",
     "summary": "Integrate `product_template_multi_link_date_span` in Shopinvader",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "author": "Camptocamp",
