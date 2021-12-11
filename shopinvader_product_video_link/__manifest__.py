9e1b79a840757ffd2dca1fbfe875ed73a881aa74
Sébastien BEAU <sebastien.beau@akretion.com>
2021-12-11 17:08:06 +0100

    [IMP] add shopinvader_product_video_link

---
+++ shopinvader_product_video_link/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Product Video Link",
+    "summary": "Add video on your Shopinvader website",
+    "version": "14.0.1.0.0",
+    "category": "Shopinvader",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "shopinvader",
+        "product_video_link",
+    ],
+    "data": [
+        "data/ir_export_product.xml",
+        "data/ir_export_category.xml",
+    ],
+    "demo": [],
+}
