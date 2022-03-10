5f9dae79d55b4fa4128e09b33bc463d9de3710df
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2022-03-10 12:23:55 +0100

    [14.0][FIX] shopinvader - problem when creating a shopinvader.backend
    a wrong default value on shopinvader.backend -> notification_ids
    when creating a new backend, all existing one are emptied of nofitication and all notification ends on the new one

--- shopinvader/models/shopinvader_backend.py
+++ shopinvader/models/shopinvader_backend.py
@@ -36,7 +36,6 @@ class ShopinvaderBackend(models.Model):
         "shopinvader.notification",
         "backend_id",
         "Notification",
-        default=lambda self: self.env["shopinvader.notification"].search([]),
     )
     nbr_product = fields.Integer(
         compute="_compute_nbr_content", string="Number of bound products"
