54e0d033be84d599c4d1ae2189d7f14dac6564af
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-03-16 16:05:58 -0300

    [ADD] shopinvader_sale_exception

---
+++ shopinvader_sale_exception/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Sale Exception",
+    "summary": "Expose sale exceptions to Shopinvader",
+    "version": "14.0.1.0.0",
+    "author": "Camptocamp SA",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "license": "AGPL-3",
+    "category": "Shopinvader",
+    "depends": ["shopinvader", "sale_exception"],
+}
---
+++ shopinvader_sale_exception/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module integrates ``sale_exception`` with Shopinvader.
+
+Exceptions can be checked by the Shop using the ``check_exceptions`` endpoint
+on the ``cart`` service.
