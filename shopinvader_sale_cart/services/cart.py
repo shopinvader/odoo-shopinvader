80a85413377d4d19fc42d229ffbdded3a80ccfa5
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-03-21 17:44:35 +0100

    [ADD] shopinvader_sale_cart: Glue module between shopinvader and sale_cart_rest_api

---
+++ shopinvader_sale_cart/services/cart.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class CartService(Component):
+    _inherit = ["base.shopinvader.service", "sale.cart.service"]
+    _name = "sale.cart.service"
+    _usage = "cart"
+    _collection = "shopinvader.api.v2"
+
+    @property
+    def _address_output_schema(self):
+        schema = super(CartService, self)._address_output_schema
+        schema["address_type"] = {
+            "type": "string",
+            "required": True,
+            "nullable": False,
+            "allowed": [
+                s
+                for s in self.env["res.partner"]
+                ._fields["address_type"]
+                .get_values(self.env)
+            ],
+        }
+        return schema
+
+    @property
+    def _json_address_parser(self):
+        parser = super(CartService, self)._json_address_parser
+        parser.append("address_type")
+        return parser
+
+    @property
+    def anonymous_partner(self):
+        return self.shopinvader_backend.anonymous_partner_id
+
+    def _get_open_cart_domain(self, uuid=None):
+        domain = super(CartService, self)._get_open_cart_domain(uuid=uuid)
+        domain.append(
+            ("shopinvader_backend_id", "=", self.shopinvader_backend.id)
+        )
+        return domain
+
+    def _prepare_cart(self):
+        vals = super(CartService, self)._prepare_cart()
+        vals["shopinvader_backend_id"] = self.shopinvader_backend.id
+        if self.shopinvader_backend.sequence_id:
+            vals["name"] = self.shopinvader_backend.sequence_id._next()
+        if self.shopinvader_backend.account_analytic_id.id:
+            vals[
+                "project_id"
+            ] = self.shopinvader_backend.account_analytic_id.id
+        if self.shopinvader_backend.pricelist_id:
+            # We must always force the pricelist. In the case of sale_profile
+            # the pricelist is not set on the backend
+            vals.update(
+                {"pricelist_id": self.shopinvader_backend.pricelist_id.id}
+            )
+        return vals
+
+    def _get_default_pricelist_id(self):
+        return self.shopinvader_backend.pricelist_id.id
+
+    def _convert_cart_to_json(self, sale):
+        vals = super(CartService, self)._convert_cart_to_json(sale)
+        vals.update(
+            {
+                "state_label": self._get_selection_label(
+                    sale, "shopinvader_state"
+                ),
+                "state": sale.shopinvader_state,
+            }
+        )
+        return vals
