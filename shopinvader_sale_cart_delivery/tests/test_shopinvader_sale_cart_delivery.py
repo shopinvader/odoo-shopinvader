667b5fb4bdf426f1b27104c9dfc429fa9e14b266
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-03-30 11:23:33 +0200

    [ADD] shopinvader_sale_cart_delivery: New addon to manage delivery on sale cart

---
+++ shopinvader_sale_cart_delivery/tests/test_shopinvader_sale_cart_delivery.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from contextlib import contextmanager
+
+import mock
+
+from odoo.addons.base_rest.controllers.main import _PseudoCollection
+from odoo.addons.component.core import WorkContext
+from odoo.addons.shopinvader.tests.common import CommonCase
+
+
+class TestShopinvaderSaleCartDelivery(CommonCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestShopinvaderSaleCartDelivery, cls).setUpClass()
+        cls.product_1 = cls.env.ref("product.product_product_4b")
+        cls.partner = cls.env.ref("shopinvader.partner_1")
+        cls.poste_carrier = cls.env.ref("delivery.delivery_carrier")
+
+        with cls.cart_service(authenticated_partner_id=cls.partner.id) as cart:
+            info = cart.sync(
+                uuid=None,
+                transactions=[
+                    {
+                        "uuid": "uuid1",
+                        "product_id": cls.product_1.id,
+                        "qty": 1,
+                    }
+                ],
+            )
+            cls.so = cls.env["sale.order"].browse(info["id"])
+            cls.cart_service = cart
+
+    @classmethod
+    @contextmanager
+    def cart_service(cls, authenticated_partner_id):
+        env = cls.env(
+            context=dict(
+                cls.env.context,
+                authenticated_partner_id=authenticated_partner_id,
+            )
+        )
+        collection = _PseudoCollection("shopinvader.api.v2", env)
+        work = WorkContext(
+            model_name="rest.service.registration",
+            collection=collection,
+            request=mock.Mock(),
+            authenticated_partner_id=authenticated_partner_id,
+            shopinvader_backend=cls.backend,
+        )
+        yield work.component(usage="cart")
+
+    def test_set_carrier(self):
+        info = self.cart_service.dispatch(
+            "set_carrier", params={"carrier_id": self.poste_carrier.id}
+        )
+        self.assertTrue(info)
+        self.assertIn("delivery", info)
+        self.assertEqual(self.poste_carrier.id, info["delivery"]["id"])
