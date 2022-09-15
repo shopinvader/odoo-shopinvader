d1af4493e67724cfc0dfb29c19736c2182fbbe7f
François Honoré <francois.honore@acsone.eu>
2022-09-15 15:04:46 +0200

    [FIX] Minor fixes

--- shopinvader_backend_image_proxy/tests/test_shopinvader_backend.py
+++ shopinvader_backend_image_proxy/tests/test_shopinvader_backend.py
@@ -38,47 +38,47 @@ class TestShopinvaderBackendTest(ProductCommonCase, ProductImageCommonCase):
             }
         )
 
-    def _test_url(self, input, output, proxy):
+    def _test_url(self, input_val, output, proxy):
         if proxy:
             self.backend.image_proxy_url = proxy
-        self.assertEqual(output, self.backend._replace_by_proxy(input))
+        self.assertEqual(output, self.backend._replace_by_proxy(input_val))
 
     def test_replace_url(self):
         self._test_url(
-            input="https://example.com/img.png",
+            input_val="https://example.com/img.png",
             output="https://test.com/img.png",
             proxy="https://test.com",
         )
 
     def test_replace_url_with_params(self):
         self._test_url(
-            input="https://example.com/img.png?foo=bar",
+            input_val="https://example.com/img.png?foo=bar",
             output="https://test.com/img.png?foo=bar",
             proxy="https://test.com",
         )
 
     def test_replace_url_with_subdomain(self):
         self._test_url(
-            input="https://sub.example.com/abc/img.png",
+            input_val="https://sub.example.com/abc/img.png",
             output="https://test.com/img.png",
             proxy="https://test.com",
         )
         self._test_url(
-            input="https://example.com/img.png",
+            input_val="https://example.com/img.png",
             output="https://sub.test.com/abc/img.png",
             proxy="https://sub.test.com/abc",
         )
 
     def test_replace_url_scheme(self):
         self._test_url(
-            input="http://example.com/img.png",
+            input_val="http://example.com/img.png",
             output="https://test.com/img.png",
             proxy="https://test.com",
         )
 
     def test_do_not_replace_url(self):
         self._test_url(
-            input="https://example.com/img.png",
+            input_val="https://example.com/img.png",
             output="https://example.com/img.png",
             proxy=None,
         )
