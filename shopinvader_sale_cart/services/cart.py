1d19b2bbb5cb462536af061bef87e3b13ca5399b
Quentin Groulard <quentin.groulard@acsone.eu>
2022-12-09 14:36:34 +0100

    [IMP] shopinvader_sale_cart: black, isort, prettier

--- shopinvader_sale_cart/services/cart.py
+++ shopinvader_sale_cart/services/cart.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -17,9 +16,7 @@ class CartService(Component):
 
     def _get_open_cart_domain(self, uuid=None):
         domain = super(CartService, self)._get_open_cart_domain(uuid=uuid)
-        domain.append(
-            ("shopinvader_backend_id", "=", self.shopinvader_backend.id)
-        )
+        domain.append(("shopinvader_backend_id", "=", self.shopinvader_backend.id))
         return domain
 
     def _prepare_cart(self):
@@ -28,15 +25,11 @@ class CartService(Component):
         if self.shopinvader_backend.sequence_id:
             vals["name"] = self.shopinvader_backend.sequence_id._next()
         if self.shopinvader_backend.account_analytic_id.id:
-            vals[
-                "project_id"
-            ] = self.shopinvader_backend.account_analytic_id.id
+            vals["project_id"] = self.shopinvader_backend.account_analytic_id.id
         if self.shopinvader_backend.pricelist_id:
             # We must always force the pricelist. In the case of sale_profile
             # the pricelist is not set on the backend
-            vals.update(
-                {"pricelist_id": self.shopinvader_backend.pricelist_id.id}
-            )
+            vals.update({"pricelist_id": self.shopinvader_backend.pricelist_id.id})
         return vals
 
     def _get_default_pricelist_id(self):
@@ -46,9 +39,7 @@ class CartService(Component):
         vals = super(CartService, self)._convert_cart_to_json(sale)
         vals.update(
             {
-                "state_label": self._get_selection_label(
-                    sale, "shopinvader_state"
-                ),
+                "state_label": self._get_selection_label(sale, "shopinvader_state"),
                 "state": sale.shopinvader_state,
             }
         )
