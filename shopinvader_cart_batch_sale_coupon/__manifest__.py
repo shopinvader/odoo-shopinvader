0fb54f17bec906f9b8f732d9c13cb8d6332ce45a
Florian Mounier <paradoxxx.zero@gmail.com>
2023-06-06 17:42:12 +0200

    [ADD] shopinvader_cart_batch_sale_coupon

---
+++ shopinvader_cart_batch_sale_coupon/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Akretion - Florian Mounier
+{
+    "name": "Shopinvader Cart Batch Sale Coupon",
+    "summary": "This module overrides sale coupon skip_coupon_recompute "
+    "for cart batch in order to compute promotion only once.",
+    "version": "14.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["shopinvader_cart_batch", "shopinvader_sale_coupon"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "auto_install": True,
+}
