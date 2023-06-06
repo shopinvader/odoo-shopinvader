a0f9e8ca1ac4b2f38ffa51a05c5759d7b975adf8
mle <marie.lejeune@acsone.eu>
2023-06-06 13:58:05 +0200

    [MIG] sale_cart: Migration to 16.0

--- sale_cart/__manifest__.py
+++ sale_cart/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Uee Sale Orders as cart.
         """,
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
