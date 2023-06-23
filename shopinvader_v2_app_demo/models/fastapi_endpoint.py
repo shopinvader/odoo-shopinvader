9ecbd229289a18c75fe9fe4d00e2ec9bc7844de7
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2023-06-23 17:01:17 +0200

    shopinvader_v2_app_demo: link swagger to demo keycloak

--- shopinvader_v2_app_demo/models/fastapi_endpoint.py
+++ shopinvader_v2_app_demo/models/fastapi_endpoint.py
@@ -5,12 +5,14 @@ from functools import partial
 from typing import Any, List
 
 from fastapi import APIRouter, FastAPI
+from fastapi.security import OAuth2AuthorizationCodeBearer
 
 from odoo import api, fields, models
 
 from odoo.addons.fastapi.dependencies import authenticated_partner_impl
 from odoo.addons.fastapi_auth_jwt.dependencies import (
     auth_jwt_default_validator_name,
+    auth_jwt_http_header_authorization,
 )
 from odoo.addons.shopinvader_api_address.routers.address_service import address_router
 from odoo.addons.shopinvader_api_cart.routers import cart_router
@@ -68,17 +70,34 @@ class FastapiEndpoint(models.Model):
         params = super()._prepare_fastapi_app_params()
         if self.app == "shopinvader_demo":
             params["openapi_tags"] = self._get_shopinvader_demo_tags(params)
+            params[
+                "swagger_ui_oauth2_redirect_url"
+            ] = "/shopinvader_demo/docs/oauth2-redirect"
         return params
 
     def _get_app(self):
         app = super()._get_app()
         if self.app == "shopinvader_demo":
+            oauth2_scheme = OAuth2AuthorizationCodeBearer(
+                authorizationUrl=(
+                    "https://keycloak.demo16.shopinvader.com/"
+                    "auth/realms/master/protocol/openid-connect/auth"
+                ),
+                tokenUrl=(
+                    "https://keycloak.demo16.shopinvader.com/"
+                    "auth/realms/master/protocol/openid-connect/token"
+                ),
+                scopes={"openid": "", "email": "", "profile": ""},
+                # Don't fail if missing Authorization header, as we look for the cookie too.
+                auto_error=False,
+            )
             app.dependency_overrides[
                 authenticated_partner_impl
             ] = auth_jwt_authenticated_or_anonymous_partner
             app.dependency_overrides[auth_jwt_default_validator_name] = partial(
                 lambda a: a, self.auth_jwt_validator_id.name or None
             )
+            app.dependency_overrides[auth_jwt_http_header_authorization] = oauth2_scheme
             cart_app = FastAPI()
             cart_app.include_router(cart_router)
             cart_app.dependency_overrides[
@@ -87,6 +106,9 @@ class FastapiEndpoint(models.Model):
             cart_app.dependency_overrides[auth_jwt_default_validator_name] = partial(
                 lambda a: a, self.auth_jwt_validator_id.name or None
             )
+            cart_app.dependency_overrides[
+                auth_jwt_http_header_authorization
+            ] = oauth2_scheme
             app.mount(self.root_path + "/carts", cart_app)
 
         return app
