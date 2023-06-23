dda28d29ddc312358fa1300d8e1fa754d91e1f2f
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2023-06-23 11:04:00 +0200

    update schema

--- shopinvader_schema_address/schemas.py
+++ shopinvader_schema_address/schemas.py
@@ -25,32 +25,31 @@ class Address(BaseModel, metaclass=ExtendableModelMeta):
 
     @classmethod
     def from_res_partner(cls, odoo_rec):
-        res = cls.construct()
-
-        res.id = odoo_rec.id
-        res.name = odoo_rec.name or None
-        res.street = odoo_rec.street or None
-        res.street2 = odoo_rec.street or None
-        res.zip = odoo_rec.zip or None
-        res.city = odoo_rec.city or None
-        res.phone = odoo_rec.phone or None
-        res.email = odoo_rec.email or None
-        res.state_id = odoo_rec.state_id.id or None
-        res.country_id = odoo_rec.country_id.id or None
-
-        return res
+        return cls(
+            id=odoo_rec.id,
+            name=odoo_rec.name or None,
+            street=odoo_rec.street or None,
+            street2=odoo_rec.street2 or None,
+            zip=odoo_rec.zip or None,
+            city=odoo_rec.city or None,
+            phone=odoo_rec.phone or None,
+            email=odoo_rec.email or None,
+            state_id=odoo_rec.state_id.id or None,
+            country_id=odoo_rec.country_id.id or None,
+        )
 
 
 class BillingAddress(Address):
     """
     Billing Address
     """
+
     vat: str | None
 
     @classmethod
     def from_res_partner(cls, odoo_rec):
         res = super().from_res_partner(odoo_rec)
-        res.vat = odoo_rec.vat
+        res.vat = odoo_rec.vat or None
 
         return res
 
