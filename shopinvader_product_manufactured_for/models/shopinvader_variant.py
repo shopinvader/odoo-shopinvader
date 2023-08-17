4a9bb5485c8e478627372e4ed546744f1a82db5b
Simone Orsi <simahawk@gmail.com>
2023-08-17 15:19:50 +0200

    s_product_manufactured_for: adapt to variant
    Partner relation has been moved to variant on https://github.com/OCA/product-attribute/pull/1400

--- shopinvader_product_manufactured_for/models/shopinvader_variant.py
+++ shopinvader_product_manufactured_for/models/shopinvader_variant.py
@@ -14,23 +14,20 @@ class ShopinvaderVariant(models.Model):
     manufactured_for_partners = Serialized(compute="_compute_manufactured_for_partners")
 
     def _compute_manufactured_for_partners(self):
-        pids = self.mapped("product_tmpl_id").ids
+        pids = self.mapped("record_id").ids
         # Use raw sql because accessing the m2m can be very slow
         query = """
             SELECT
-                product_template_id,ARRAY_AGG(res_partner_id)
+                product_id,ARRAY_AGG(partner_id)
             FROM
                 %s
             WHERE
-                product_template_id in %s
+                product_id in %s
             GROUP BY
-                product_template_id
+                product_id
         """
-        rel_table = (
-            self.env["product.template"]
-            ._fields["manufactured_for_partner_ids"]
-            .relation
-        )
+        prod_model = self.env["product.product"]
+        rel_table = prod_model._fields["manufactured_for_partner_ids"].relation
         self.env.cr.execute(query, (AsIs(rel_table), tuple(pids)))
         mapping = dict(self.env.cr.fetchall())
         for record in self:
@@ -39,5 +36,16 @@ class ShopinvaderVariant(models.Model):
             # On algolia you cannot filter by given value and EMPTY value
             # at the same time. Hence, we set a default value for no value.
             record.manufactured_for_partners = mapping.get(
-                record.product_tmpl_id.id, ["_NOVALUE_"]
+                record.record_id.id, ["_NOVALUE_"]
             )
+
+    def _redirect_existing_url(self):
+        # Avoid redirection of product manufactured for specific partners.
+        # Such products are not public thus is useless to redirect their URLs
+        # to the parent category to preserve SEO links.
+        # Moreover, this makes the index record size grow
+        # which is not nice (especially w/ Algolia that has a limited size).
+        to_not_redirect = self.filtered(lambda x: x.manufactured_for_partner_ids)
+        return super(
+            ShopinvaderVariant, self - to_not_redirect
+        )._redirect_existing_url()
