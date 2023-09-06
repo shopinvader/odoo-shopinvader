e424498681b1d8a4f1f57dc8547dd13cf1688ef7
Sébastien BEAU <sebastien.beau@akretion.com>
2023-09-06 23:31:57 +0200

    shopinvader_fastapi_auth_partner: add module

---
+++ shopinvader_fastapi_auth_partner/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Shopinvader Auth Partner authentication for FastAPI endpoints",
+    "summary": """
+        Provide Partner and Anonymous Partner authentication to FastAPI routes.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "maintainers": [],
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["fastapi_auth_partner", "shopinvader_anonymous_partner"],
+    "data": [],
+    "demo": [],
+}
