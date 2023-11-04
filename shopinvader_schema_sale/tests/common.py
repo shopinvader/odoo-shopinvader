e0c229f0172ebf6ef7b82d03f6049003ca24bc4f
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-11-04 08:58:25 +0100

    [IMP] shopinvader_schema_sale: Add simple tests

---
+++ shopinvader_schema_sale/tests/common.py
@@ -0,0 +1,45 @@
+# Copyright 2023 ACSONE SA/NV (https://acsone.eu).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+from odoo.addons.extendable.tests.common import ExtendableMixin
+
+
+class SchemaSaleCase(TransactionCase, ExtendableMixin):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        super().init_extendable_registry()
+
+        cls.partner = cls.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+                "email": "test@test.eu",
+            }
+        )
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Test Product",
+                "list_price": 10.0,
+            }
+        )
+        cls.sale_order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "client_order_ref": "client ref",
+                "note": "note",
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 1,
+                            "price_unit": 10.0,
+                        },
+                    )
+                ],
+            }
+        )
---
+++ shopinvader_schema_sale/tests/test_sale_schema.py
@@ -0,0 +1,26 @@
+# Copyright 2023 ACSONE SA/NV (https://acsone.eu).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from ..schemas import Sale, SaleSearch
+from .common import SchemaSaleCase
+
+
+class TestSaleSchema(SchemaSaleCase):
+    def test_sale_from_sale_order(self):
+        sale = Sale.from_sale_order(self.sale_order)
+        self.assertEqual(sale.id, self.sale_order.id)
+        self.assertEqual(sale.state, self.sale_order.state)
+        self.assertEqual(sale.name, self.sale_order.name)
+        self.assertEqual(sale.client_order_ref, "client ref")
+        self.assertEqual(sale.date_order, self.sale_order.date_order)
+        self.assertEqual(sale.date_commitment, None)
+        self.assertEqual(sale.typology, self.sale_order.typology)
+        self.assertEqual(sale.note, "<p>note</p>")
+        self.assertEqual(len(sale.lines), 1)
+        self.assertEqual(sale.lines[0].id, self.sale_order.order_line[0].id)
+        self.assertEqual(sale.lines[0].qty, 1)
+
+    def test_domain_from_sale_search(self):
+        search = SaleSearch(name="test")
+        domain = search.to_odoo_domain(self.env)
+        self.assertIn(("name", "ilike", "test"), domain)
