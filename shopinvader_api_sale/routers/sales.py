22eeb7209b8e0588978e598e184cf4bb5a0ff997
Sébastien BEAU <sebastien.beau@akretion.com>
2023-11-06 11:03:06 +0100

    shopinvader_api_sale: fix comment

--- shopinvader_api_sale/routers/sales.py
+++ shopinvader_api_sale/routers/sales.py
@@ -9,13 +9,8 @@ from fastapi import APIRouter, Depends
 
 from odoo import api, models
 
-from odoo.addons.base.models.res_partner import Partner as ResPartner
 from odoo.addons.extendable_fastapi.schemas import PagedCollection
-from odoo.addons.fastapi.dependencies import (
-    authenticated_partner,
-    authenticated_partner_env,
-    paging,
-)
+from odoo.addons.fastapi.dependencies import authenticated_partner_env, paging
 from odoo.addons.fastapi.schemas import Paging
 from odoo.addons.fastapi.utils import FilteredDomainAdapter
 from odoo.addons.shopinvader_schema_sale.schemas import Sale, SaleSearch
@@ -28,9 +23,9 @@ def search(
     params: Annotated[SaleSearch, Depends()],
     paging: Annotated[Paging, Depends(paging)],
     env: Annotated[api.Environment, Depends(authenticated_partner_env)],
-    partner: Annotated["ResPartner", Depends(authenticated_partner)],
 ) -> PagedCollection[Sale]:
-    """Get the list of sale orders."""
+    """Get / search sale orders. The list contains only sale orders from the
+    authenticated user"""
     count, orders = env["shopinvader_api_sale.sales_router.helper"]._search(
         paging, params
     )
@@ -44,11 +39,9 @@ def search(
 def get(
     sale_id: int,
     env: Annotated[api.Environment, Depends(authenticated_partner_env)],
-    partner: Annotated["ResPartner", Depends(authenticated_partner)],
 ) -> Sale:
     """
     Get sale order of authenticated user with specific sale_id
-    sale corresponds to authenticated partner
     """
     return Sale.from_sale_order(
         env["shopinvader_api_sale.sales_router.helper"]._get(sale_id)
@@ -60,16 +53,16 @@ class ShopinvaderApiSaleSalesRouterHelper(models.AbstractModel):
     _description = "Shopinvader Api Sale Service Helper"
 
     @property
-    def adapter(self):
+    def filtered_model(self):
         return FilteredDomainAdapter(
             self.env["sale.order"], [("typology", "=", "sale")]
         )
 
     def _get(self, record_id):
-        return self.adapter.get(record_id)
+        return self.filtered_model.get(record_id)
 
     def _search(self, paging, params):
-        return self.adapter.search_with_count(
+        return self.filtered_model.search_with_count(
             params.to_odoo_domain(self.env),
             limit=paging.limit,
             offset=paging.offset,
