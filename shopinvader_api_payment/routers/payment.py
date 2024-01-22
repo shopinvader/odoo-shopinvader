295666ea3487848e223c046f5e2cdb599468f095
mle <marie.lejeune@acsone.eu>
2024-01-22 08:52:58 +0100

    [FIX] shopinvader_api_payment: decoding the payable obj to get payment methods

--- shopinvader_api_payment/routers/payment.py
+++ shopinvader_api_payment/routers/payment.py
@@ -39,7 +39,7 @@ def pay(
     obtain the parameters with the /payment/payable route.
     """
     try:
-        payable_obj = Payable.decode(payable)
+        payable_obj = Payable.decode(odoo_env, payable)
     except Exception as e:
         _logger.info("Could not decode payable")
         raise HTTPException(403) from e
@@ -78,7 +78,7 @@ def transaction(
     user. This route is public, so it is possible to pay anonymously.
     """
     try:
-        payable_obj = Payable.decode(data.payable)
+        payable_obj = Payable.decode(odoo_env, data.payable)
     except Exception as e:
         _logger.info("Could not decode payable")
         raise HTTPException(403) from e
