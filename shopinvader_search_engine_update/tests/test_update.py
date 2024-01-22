ff9671a57734e82b3690b5835f85e22625dbde00
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-01-22 16:40:33 +0100

    [FIX] shopinvader_search_engine_update: Archived product update must not mark binding to be updated

--- shopinvader_search_engine_update/tests/test_update.py
+++ shopinvader_search_engine_update/tests/test_update.py
@@ -12,3 +12,13 @@ class TestUpdate(TestProductBindingUpdateBase):
     def test_update_template(self):
         self.product.product_tmpl_id.name = "new name"
         self.assertEqual(self.product_binding.state, "to_recompute")
+
+    def test_update_on_an_archived_product(self):
+        self.product.active = False
+        self.product_binding.state = "to_delete"
+        self.product.name = "new name"
+        self.assertEqual(
+            self.product_binding.state,
+            "to_delete",
+            "The product binding should not be updated if an archived product is updated",
+        )
