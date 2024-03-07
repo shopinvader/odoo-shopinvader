350b3d535e4d1f940dbef131d4166d8246d45d03
Florian Mounier <paradoxxx.zero@gmail.com>
2024-03-07 11:56:13 +0100

    [LINT] shopinvader_unit_management, shopinvader_api_unit_member, shopinvader_api_unit_request

--- shopinvader_api_unit_request/routers/unit_request_lines.py
+++ shopinvader_api_unit_request/routers/unit_request_lines.py
@@ -16,7 +16,6 @@ from odoo.addons.fastapi.dependencies import (
     paging,
 )
 from odoo.addons.fastapi.schemas import Paging
-
 from odoo.addons.sale.models.sale_order_line import SaleOrderLine
 from odoo.addons.shopinvader_api_unit_member.routers.unit_members import (
     authenticated_manager,
