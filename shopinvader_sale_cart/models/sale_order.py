a609ec8117d0024e3125a136c0dee2ce83425bdc
Florian Mounier <paradoxxx.zero@gmail.com>
2024-11-07 14:18:52 +0100

    [FIX] shopinvader_sale_cart: Fix bad rebase, update _get_cart_line signature

--- shopinvader_sale_cart/models/sale_order.py
+++ shopinvader_sale_cart/models/sale_order.py
@@ -6,7 +6,6 @@ from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-
     _inherit = "sale.order"
 
     uuid = fields.Char(string="EShop Unique identifier", readonly=True)
@@ -93,7 +92,9 @@ class SaleOrder(models.Model):
         self.ensure_one()
         update_cmds = []
         for cart_line in cart.order_line:
-            line = self._get_cart_line(cart_line.product_id.id)
+            line = self._get_cart_line(
+                cart_line.product_id.id, {}
+            )  # FIXME pass options
             if line:
                 new_qty = line.product_uom_qty + cart_line.product_uom_qty
                 vals = {"product_uom_qty": new_qty}
