efecf1738315d7404809d4561e67924a42bf9c88
Florian Mounier <paradoxxx.zero@gmail.com>
2024-12-12 11:24:01 +0100

    [IMP] shopinvader_fastapi_auth_partner: Adapt according to https://github.com/shopinvader/odoo-shopinvader/pull/1570/commits/98f70b64d37c1b9496db09a05099aa613b442e5a

---
+++ shopinvader_fastapi_auth_partner/helper.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AuthService(models.AbstractModel):
+    _inherit = "fastapi.auth.service"
+
+    def _set_auth_cookie(self, auth_partner, request, response):
+        rv = super(AuthService, self)._set_auth_cookie(auth_partner, request, response)
+        # Handle anonymous partner
+        self.env["res.partner"]._promote_anonymous_partner(
+            auth_partner.partner_id, request.cookies, response
+        )
+        return rv
