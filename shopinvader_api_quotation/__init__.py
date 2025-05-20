c23d91ac774b9342fe57b00ffbedc7724d67d896
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2025-05-20 14:57:33 +0200

    [IMP] shopinvader_api_quotation: remove sale_cart dependency and enhance API
    - put shopinvader_sale_state as a dependency in order to remove sale_cart because we need the field sale.order.shopinvader_state
    
    This commit introduces significant enhancements to the REST API for quotation management, including:
    
    - **Create Method:** Added a new endpoint to create quotations.
    - **Line Management Methods:** Implemented dedicated endpoints for adding, deleting, or updating single or multiple lines within a quotation.
    - **Update Extension:** Extended the existing update method to support the addition of new lines

--- shopinvader_api_quotation/__init__.py
+++ shopinvader_api_quotation/__init__.py
@@ -1,1 +1,4 @@
 from . import routers
+from . import dependencies
+from . import schemas
+from . import models
--- shopinvader_api_quotation/schemas/sale.py
+++ shopinvader_api_quotation/schemas/sale.py
@@ -1,4 +1,8 @@
+from typing import Generic, TypeVar
+
 from extendable_pydantic import StrictExtendableBaseModel
+
+from odoo import Command
 
 from odoo.addons.shopinvader_schema_sale.schemas.sale import Sale
 
@@ -13,12 +17,63 @@ class Sale(Sale, extends=True):
         return res
 
 
-class QuotationUpdateInput(StrictExtendableBaseModel, extra="ignore"):
-    client_order_ref: str | None = None
-
-    def to_sale_order_vals(self) -> dict:
-        return {"client_order_ref": self.client_order_ref}
-
-
 class QuotationConfirmInput(StrictExtendableBaseModel):
     """Extend it if you need params for the confirmation"""
+
+
+class QuotationLineCreateRequest(StrictExtendableBaseModel, extra="ignore"):
+    sequence: int | None = None
+    product_id: int
+    quantity: float
+
+
+class QuotationCreateRequest(StrictExtendableBaseModel, extra="ignore"):
+    name: str
+    lines: list[QuotationLineCreateRequest] | None = None
+    client_order_ref: str | None = None
+    typology: str
+
+
+class QuotationAddLineRequest(StrictExtendableBaseModel, extra="ignore"):
+    sequence: int | None = None
+    product_id: int
+    quantity: float
+
+
+class QuotationUpdateLineRequest(StrictExtendableBaseModel, extra="ignore"):
+    line_id: int
+    sequence: int | None = None
+    product_id: int
+    quantity: float
+
+
+class QuotationDeleteLineRequest(StrictExtendableBaseModel, extra="ignore"):
+    line_id: int
+
+
+T = TypeVar("T")
+
+
+class QuotationLines(StrictExtendableBaseModel, Generic[T]):
+    lines: list[T] = []
+
+
+class QuotationUpdateInput(StrictExtendableBaseModel, extra="ignore"):
+    client_order_ref: str | None = None
+    lines: list[QuotationUpdateLineRequest] | None = None
+
+    def to_sale_order_vals(self) -> dict:
+        return {
+            "client_order_ref": self.client_order_ref,
+            "order_line": [
+                Command.update(
+                    line.line_id,
+                    {
+                        "product_id": line.product_id,
+                        "product_uom_qty": line.quantity,
+                        "sequence": line.sequence,
+                    },
+                )
+                for line in self.lines
+            ],
+        }
