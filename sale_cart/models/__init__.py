267022307eb8d42effdd3a2b94f5488b09e8fa81
Florian Mounier <paradoxxx.zero@gmail.com>
2025-06-05 10:34:46 +0200

    [IMP] sale_cart: Automatically convert cart to sale when a payment is authorized, set as pending, or done.

--- sale_cart/models/__init__.py
+++ sale_cart/models/__init__.py
@@ -1,1 +1,2 @@
+from . import payment_transaction
 from . import sale_order
--- sale_cart/tests/test_sale_cart.py
+++ sale_cart/tests/test_sale_cart.py
@@ -1,35 +1,11 @@
 # Copyright 2022 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
+
+from .common import SaleCartCommon
 
 
-class TestSaleCart(TransactionCase):
-    @classmethod
-    def setUpClass(cls):
-        super(TestSaleCart, cls).setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.product = cls.env["product.product"].create(
-            {
-                "name": "product",
-                "uom_id": cls.env.ref("uom.product_uom_unit").id,
-            }
-        )
-        cls.partner = cls.env["res.partner"].create({"name": "partner"})
-        cls.so_cart = cls.env["sale.order"].create(
-            {
-                "partner_id": cls.partner.id,
-                "order_line": [
-                    (
-                        0,
-                        0,
-                        {"product_id": cls.product.id, "product_uom_qty": 1},
-                    )
-                ],
-                "typology": "cart",
-            }
-        )
-
+class TestSaleCart(SaleCartCommon):
     def test_action_confirm_cart(self):
         self.assertEqual("draft", self.so_cart.state)
         self.assertEqual("cart", self.so_cart.typology)
