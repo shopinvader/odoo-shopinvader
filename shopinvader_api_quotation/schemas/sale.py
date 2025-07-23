32d591caa686096a5746336f201ed4df0f1bb81f
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2025-07-23 12:29:43 +0200

    [IMP] shopinvader_api_quotation: Add the quotation_state into the sale info

--- shopinvader_api_quotation/schemas/sale.py
+++ shopinvader_api_quotation/schemas/sale.py
@@ -2,12 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.addons.shopinvader_schema_sale.schemas.sale import Sale
 
+from .quotation import QuotationState
+
 
 class Sale(Sale, extends=True):
     shop_only_quotation: bool | None = None
+    quotation_state: QuotationState | None = None
 
     @classmethod
     def from_sale_order(cls, odoo_rec):
         res = super().from_sale_order(odoo_rec)
         res.shop_only_quotation = odoo_rec.shop_only_quotation
+        res.quotation_state = (
+            QuotationState(odoo_rec.quotation_state)
+            if odoo_rec.quotation_state
+            else None
+        )
         return res
