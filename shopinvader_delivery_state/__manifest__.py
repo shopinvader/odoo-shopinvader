72afd249b4b3b177105448c1d720c73785d7806d
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-24 10:06:52 +0100

    [MIG] shopinvader_delivery_state

--- shopinvader_delivery_state/__manifest__.py
+++ shopinvader_delivery_state/__manifest__.py
@@ -3,15 +3,13 @@
 
 {
     "name": "Shopinvader delivery state",
-    "Summary": """
-        Integrate `sale_delivery_state` with Shopinvader
-    """,
-    "version": "16.0.1.0.1",
+    "Summary": "Integrate `sale_delivery_state` with Shopinvader",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
     "depends": ["shopinvader_sale_state", "sale_delivery_state"],
     "auto_install": True,
     "post_init_hook": "post_init_hook",
-    'installable': False,
+    "installable": True,
 }
--- shopinvader_delivery_state/hooks.py
+++ shopinvader_delivery_state/hooks.py
@@ -3,12 +3,9 @@
 
 import logging
 
-from odoo import SUPERUSER_ID, api
-
 _logger = logging.getLogger(__name__)
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     _logger.info("Recompute sale.order.shopinvader_state")
-    env = api.Environment(cr, SUPERUSER_ID, {})
     env["sale.order"].search([])._compute_shopinvader_state()
