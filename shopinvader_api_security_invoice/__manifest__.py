a6cbb4f9e37af270d78997e5d374118f718d9ed8
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-24 10:06:36 +0100

    [MIG] shopinvader_api_security_invoice

--- shopinvader_api_security_invoice/__manifest__.py
+++ shopinvader_api_security_invoice/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Api Security invoice",
     "summary": "Add security rule to expose invoices",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Uncategorized",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
@@ -23,5 +23,5 @@
         "security/rule+acl_account_move.xml",
         "security/rule+acl_account_move_line.xml",
     ],
-    'installable': False,
+    "installable": True,
 }
