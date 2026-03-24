4a3fabf0978a3aada9c8adae33dea9721d4bb92d
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-24 10:06:01 +0100

    [MIG] shopinvader_api_address_shipping_note

--- shopinvader_api_address_shipping_note/__manifest__.py
+++ shopinvader_api_address_shipping_note/__manifest__.py
@@ -4,8 +4,9 @@
 {
     "name": "Shopinvader Api Address Shipping Note",
     "summary": """
-        Adds a shipping note on schemas of services to manage Shipping Addresses""",
-    "version": "16.0.1.0.0",
+        Adds a shipping note on schemas of services to manage Shipping Addresses
+    """,
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV",
     "website": "https://github.com/shopinvader/odoo-shopinvader",
@@ -13,7 +14,5 @@
         "shopinvader_api_address",
         "shopinvader_address_shipping_note",
     ],
-    "data": [],
-    "demo": [],
-    'installable': False,
+    "installable": True,
 }
