1d1aee0874a924a398afb7133ba55e4d6ba9abfb
Florian Mounier <paradoxxx.zero@gmail.com>
2026-03-24 10:06:23 +0100

    [MIG] shopinvader_api_lead: Migration to 18.0

--- shopinvader_api_lead/routers/lead.py
+++ shopinvader_api_lead/routers/lead.py
@@ -7,16 +7,34 @@ from fastapi import APIRouter, Depends
 from odoo import api
 
 from odoo.addons.fastapi.dependencies import odoo_env
+from odoo.addons.shopinvader_router_helper import VirtualModel
 
 from ..schemas import Lead, LeadInput
 
 lead_router = APIRouter(tags=["leads"])
 
 
+class LeadHelper(VirtualModel):
+    _inherit = "shopinvader.router.helper"
+    _name = "shopinvader_api_lead.routers.helper"
+    _description = "Shopinvader API Lead Router Helper"
+
+    _model = "crm.lead"
+
+    def _domain(self):
+        return []
+
+
+def lead_helper(
+    env: Annotated[api.Environment, Depends(odoo_env)],
+):
+    return env["shopinvader_api_lead.routers.helper"].new()
+
+
 @lead_router.post("/leads", status_code=201)
 def create(
     data: LeadInput,
-    env: Annotated[api.Environment, Depends(odoo_env)],
+    helper: Annotated[LeadHelper, Depends(lead_helper)],
 ) -> Lead | None:
-    lead = env["crm.lead"].create(data.to_crm_lead_vals())
+    lead = helper.create(data.to_crm_lead_vals())
     return Lead.from_crm_lead(lead)
